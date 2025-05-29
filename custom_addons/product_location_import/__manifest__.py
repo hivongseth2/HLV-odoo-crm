9a8dc9114278b25b22b3645d2aa462cb7047797d
hivongseth2 <99702632+hivongseth2@users.noreply.github.com>
2025-05-29 15:26:27 +0700

    add module product location

---
+++ custom_addons/product_location_import/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Product Location Import",
+    "summary": "Import số lượng tồn theo vị trí từ Excel",
+    "version": "1.0",
+    "depends": ["stock"],
+    "data": [
+        "security/ir.model.access.csv"
+    ],
+    "installable": True,
+    "application": False,
+    "license": "OEEL-1"
+}
