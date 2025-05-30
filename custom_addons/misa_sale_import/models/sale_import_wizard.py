00c6a30346642bb3dacdb10782c6f52ef1ae3ece
hivongseth2 <99702632+hivongseth2@users.noreply.github.com>
2025-05-30 16:15:30 +0700

    continue if exist

--- custom_addons/misa_sale_import/models/sale_import_wizard.py
+++ custom_addons/misa_sale_import/models/sale_import_wizard.py
@@ -68,7 +68,8 @@ class SaleImportWizard(models.TransientModel):
             existing_order = self.env['sale.order'].search([('name', '=', order_ref)], limit=1)
             if existing_order:
                 _logger.warning("⛔ Đơn hàng %s đã tồn tại. Bỏ qua không tạo lại.", order_ref)
-                sale_orders[order_ref] = existing_order
+                # sale_orders[order_ref] = existing_order
+                # mở ra nếu muốn cập nhật đơn hàng đã tồn tại
                 continue
 
             sale_order = self.env['sale.order'].create({
