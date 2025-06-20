2e9a00e0cb01d26dd74dd96242828b9a4c133882
hivongseth2 <99702632+hivongseth2@users.noreply.github.com>
2025-06-20 13:32:22 +0700

    done

--- custom_addons/amis_callback/controllers/main.py
+++ custom_addons/amis_callback/controllers/main.py
@@ -8,5 +8,6 @@ class AmisCallbackController(http.Controller):
 
     @http.route('/api/amis/callback', type='json', auth='public', methods=['POST'], csrf=False)
     def amis_callback(self, **post):
-        _logger.info("Received AMIS callback: %s", post)
-        return {"Success": True}
+            _logger.info("Received AMIS callback RAW BODY: %s", request.httprequest.get_data())
+            _logger.info("Parsed JSON: %s", post)
+            return {"Success": True}
