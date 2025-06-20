f2524e9e9e9b763570c74a078a2e72fb6cda8f61
hivongseth2 <99702632+hivongseth2@users.noreply.github.com>
2025-06-20 16:16:22 +0700

    pend

--- custom_addons/misa_fetch_po_button/models/misa_po_button.py
+++ custom_addons/misa_fetch_po_button/models/misa_po_button.py
@@ -14,21 +14,13 @@ class MisaPOFetch(models.Model):
 
     def action_fetch_po(self):
         url = "https://actapp.misa.vn/g1/api/pu/v1/pu_list/paging_filter_v2"
-        access_token = "Bearer <ACCESS_TOKEN_CUA_BAN>"
+        access_token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiJkZjBjYjMzYi1iMTM5LTQ5ZjUtYWMyNC1mOWY4NjBiNGU5ODciLCJ1bmEiOiJOR1VZRU5USEFOSExVQU4iLCJhdXQiOiIwIiwidWVtIjoibmd1eWVubHVhbjEzMDMwMUBnbWFpbC5jb20iLCJuYmYiOjE3NTA0MDcwNjIsImV4cCI6MTc1MDQ5MzQ5MCwiaWF0IjoxNzUwNDA3MDYyLCJpc3MiOiJNSVNBSlNDIn0.dISLn9Vd2j5rRDWHi0wFDyfdDlk4-PeDIDHpp-5Dh4Q"
 
         headers = {
             "Authorization": access_token,
             "Content-Type": "application/json",
             "x-device": "04aadfced5b04995ecfacb0a7da5c50c",
-            "x-misa-context": json.dumps({
-                "TenantId": "47ab503b-99d5-4eb8-aa11-24927abb3585",
-                "TenantCode": "3R2PY2F4",
-                "DatabaseId": "f4b18d63-6c99-4a53-b974-f6208e84fced",
-                "BranchId": "53a073a0-5381-4493-820f-51ea32ebe990",
-                "WorkingBook": 0,
-                "Language": "vi",
-                "SessionId": "abcxyz...",
-            }),
+            "x-misa-context": {"TenantId":"47ab503b-99d5-4eb8-aa11-24927abb3585","TenantCode":"3R2PY2F4","DatabaseId":"f4b18d63-6c99-4a53-b974-f6208e84fced","BranchId":"53a073a0-5381-4493-820f-51ea32ebe990","WorkingBook":0,"Language":"vi","IncludeDependentBranch":"false","SessionId":"ssdf0cb33bb13949f5ac24f9f860b4e987.04aadfced5b04995ecfacb0a7da5c50c.f4b18d636c994a53b974f6208e84fced.638860290625845472","DBType":1,"AuthType":0,"AmisSessionId":"NAA3AGEAYgA1ADAAMwBiADkAOQBkADUANABlAGIAOABhAGEAMQAxADIANAA5ADIANwBhAGIAYgAzADUAOAA1ADAAYgBlADEAMgAyAGIAMAA3AGIANAAyADQAZAAzAGMAOQA1AGQAYQBjAGEANAAxADYAZQAxADIAMwBhADAAYQA=","HasAgent":false,"UserType":1,"art":0,"UserId":"df0cb33b-b139-49f5-ac24-f9f860b4e987","isc":false},
         }
 
         payload = {
@@ -50,7 +42,6 @@ class MisaPOFetch(models.Model):
 
         response = requests.post(url, headers=headers, json=payload)
         if response.status_code == 200:
-            _logger.info("Kết quả gọi API MISA:
-%s", json.dumps(response.json(), ensure_ascii=False, indent=2))
+            _logger.info("Kết quả gọi API MISA: %s", json.dumps(response.json(), ensure_ascii=False, indent=2))
         else:
             raise UserError(_("Gọi API thất bại: %s") % response.text)
