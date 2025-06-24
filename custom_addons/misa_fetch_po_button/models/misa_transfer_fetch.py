793d3f7ce5c7f8c48ef0f67d538de7b2f7a8bfc0
hivongseth2 <99702632+hivongseth2@users.noreply.github.com>
2025-06-24 09:01:53 +0700

    ok

--- custom_addons/misa_fetch_po_button/models/misa_transfer_fetch.py
+++ custom_addons/misa_fetch_po_button/models/misa_transfer_fetch.py
@@ -16,21 +16,22 @@ class MisaTransferFetch(models.TransientModel):
 
     def _get_misa_context(self):
         return {
-            "TenantId": "47ab503b-99d5-4eb8-aa11-24927abb3585",
-            "TenantCode": "3R2PY2F4",
-            "DatabaseId": "f4b18d63-6c99-4a53-b974-f6208e84fced",
-            "BranchId": "53a073a0-5381-4493-820f-51ea32ebe990",
-            "WorkingBook": 0,
-            "Language": "vi",
-            "IncludeDependentBranch": False,
-            "SessionId": ".",
-            "DBType": 1,
-            "AuthType": 0,
-            "AmisSessionId": "=",
-            "HasAgent": False,
-            "UserType": 1,
-            "art": 1,
-            "UserId": "df0cb33b-b139-49f5-ac24-f9f860b4e987"
+                "TenantId":"47ab503b-99d5-4eb8-aa11-24927abb3585",
+                "TenantCode":"3R2PY2F4",
+                "DatabaseId":"f4b18d63-6c99-4a53-b974-f6208e84fced",
+                "BranchId":"53a073a0-5381-4493-820f-51ea32ebe990",
+                "WorkingBook":0,
+                "Language":"vi",
+                "IncludeDependentBranch":False,
+                "SessionId":"ssdf0cb33bb13949f5ac24f9f860b4e987.04aadfced5b04995ecfacb0a7da5c50c.f4b18d636c994a53b974f6208e84fced.638860290625845472",
+                "DBType":1,
+                "AuthType":0,
+                "AmisSessionId":"NAA3AGEAYgA1ADAAMwBiADkAOQBkADUANABlAGIAOABhAGEAMQAxADIANAA5ADIANwBhAGIAYgAzADUAOAA1ADAAYgBlADEAMgAyAGIAMAA3AGIANAAyADQAZAAzAGMAOQA1AGQAYQBjAGEANAAxADYAZQAxADIAMwBhADAAYQA=",
+                "HasAgent":False,
+                "UserType":1,
+                "art":0,
+                "UserId":"df0cb33b-b139-49f5-ac24-f9f860b4e987",
+                "isc":False
         }
 
     def action_fetch_transfers(self):
