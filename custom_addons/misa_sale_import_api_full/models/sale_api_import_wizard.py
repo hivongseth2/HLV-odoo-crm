ae170202c7506eec46899d26b0c18e4ae925412e
hivongseth2 <99702632+hivongseth2@users.noreply.github.com>
2025-06-25 08:23:28 +0700

    lai

--- custom_addons/misa_sale_import_api_full/models/sale_api_import_wizard.py
+++ custom_addons/misa_sale_import_api_full/models/sale_api_import_wizard.py
@@ -42,7 +42,7 @@ class SaleApiImportWizard(models.TransientModel):
                 "page": page,
                 "pageSize": page_size,
                 "orderBy": "sale_order_date",
-                "isDescending": False
+                "isDescending": True
             }
             try:
                 response = requests.get(orders_url, headers=headers, params=params)
