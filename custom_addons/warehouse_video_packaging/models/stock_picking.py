58704837a207ada7f57a4d78d0c2be21ea3e992f
hivongseth2 <99702632+hivongseth2@users.noreply.github.com>
2025-06-27 11:57:48 +0700

    ok

--- custom_addons/warehouse_video_packaging/models/stock_picking.py
+++ custom_addons/warehouse_video_packaging/models/stock_picking.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
@@ -11,17 +11,15 @@ class StockPicking(models.Model):
 
     video_file_name = fields.Char("Tên file video")
     video_url = fields.Char("Link video Drive")
+
     @api.model
     def action_scan_barcode(self):
-        # Khi quét barcode bắt đầu quay video
         self.ensure_one()
         self.video_state = 'recording'
         self.video_url = False
-        # Gọi hàm thực hiện quay từ video_utils (đã viết trước)
         self.env['warehouse.video.packaging'].start_video_for_picking(self.id)
 
     def button_validate(self):
-        # Khi xác nhận phiếu xuất thì dừng video và upload
         res = super().button_validate()
         for picking in self:
             if picking.video_state == 'recording':
