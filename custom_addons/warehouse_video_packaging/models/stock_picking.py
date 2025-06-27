6f958c4867b9647059e3c90a95dd2d5c2d4b2239
hivongseth2 <99702632+hivongseth2@users.noreply.github.com>
2025-06-27 15:35:23 +0700

    test

--- custom_addons/warehouse_video_packaging/models/stock_picking.py
+++ custom_addons/warehouse_video_packaging/models/stock_picking.py
@@ -1,5 +1,5 @@
 from odoo import models, fields, api
-from ..tools import video_utils  # ⬅️ import file tools
+from ..tools import video_utils
 
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
@@ -13,25 +13,24 @@ class StockPicking(models.Model):
     video_file_name = fields.Char("Tên file video")
     video_url = fields.Char("Link video Drive")
 
-    _video_process = None  # Biến tạm trong Python (không lưu DB)
+    _video_process = None
 
-    def action_scan_barcode(self):
-        self.ensure_one()
-        self.video_state = 'recording'
-        self.video_file_name = f"{self.name}.mp4"
-
-        # Gọi quay
-        proc, output = video_utils.start_recording(self.name)
-        self._video_process = proc
-        self.video_url = output  # Hoặc chỉ lưu path tạm
+    def action_put_in_pack(self):
+        res = super().action_put_in_pack()
+        for picking in self:
+            if picking.video_state == 'idle':
+                picking.video_state = 'recording'
+                picking.video_file_name = f"{picking.name}.mp4"
+                proc, output = video_utils.start_recording(picking.name)
+                picking._video_process = proc
+                picking.video_url = output
+        return res
 
     def button_validate(self):
         res = super().button_validate()
         for picking in self:
             if picking.video_state == 'recording':
-                # Dừng quay
                 video_utils.stop_process(picking._video_process)
-                # Upload
                 video_utils.upload_async(picking.video_url)
                 picking.write({'video_state': 'uploaded'})
         return res
