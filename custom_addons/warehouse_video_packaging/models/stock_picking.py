a8fc2a39d6c920313ce062a26a313eaf3d758bae
hivongseth2 <99702632+hivongseth2@users.noreply.github.com>
2025-06-27 10:54:03 +0700

    ok

--- custom_addons/warehouse_video_packaging/models/stock_picking.py
+++ custom_addons/warehouse_video_packaging/models/stock_picking.py
@@ -11,3 +11,20 @@ class StockPicking(models.Model):
 
     video_file_name = fields.Char("Tên file video")
     video_url = fields.Char("Link video Drive")
+    @api.model
+    def action_scan_barcode(self):
+        # Khi quét barcode bắt đầu quay video
+        self.ensure_one()
+        self.video_state = 'recording'
+        self.video_url = False
+        # Gọi hàm thực hiện quay từ video_utils (đã viết trước)
+        self.env['warehouse.video.packaging'].start_video_for_picking(self.id)
+
+    def button_validate(self):
+        # Khi xác nhận phiếu xuất thì dừng video và upload
+        res = super().button_validate()
+        for picking in self:
+            if picking.video_state == 'recording':
+                picking.env['warehouse.video.packaging'].stop_and_upload_video(picking.id)
+                picking.write({'video_state': 'uploaded'})
+        return res
