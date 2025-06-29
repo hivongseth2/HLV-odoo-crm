6ca6619778d2e8eabc779ab3f495f7e44ea4ed7a
hivongseth2 <99702632+hivongseth2@users.noreply.github.com>
2025-06-29 16:42:37 +0700

    ok

--- custom_addons/misa_fetch_po_button/utils/odoo_utils.py
+++ custom_addons/misa_fetch_po_button/utils/odoo_utils.py
@@ -96,7 +96,7 @@ class OdooUtils(models.AbstractModel):
                 _logger.warning("⚠️ Bỏ qua dòng không hợp lệ: %s", line)
                 continue
 
-            product = self.env["odoo_utils"]._get_or_create_product(
+            product = _get_or_create_product(
                 code=product_code,
                 name=product_name,
                 unit_name=uom_name,
