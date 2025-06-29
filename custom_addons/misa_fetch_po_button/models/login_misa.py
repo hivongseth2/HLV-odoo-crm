e8b3913262c7756d31a40167f5bd6398f3671b11
hivongseth2 <99702632+hivongseth2@users.noreply.github.com>
2025-06-29 21:40:11 +0700

    ol

--- custom_addons/misa_fetch_po_button/models/login_misa.py
+++ custom_addons/misa_fetch_po_button/models/login_misa.py
@@ -8,12 +8,10 @@ _logger = logging.getLogger(__name__)
 class LoginMisa(models.TransientModel):
     _name = "login.misa"
     _description = "Login MISA"
-    username = fields.Char(string="Tài khoản", required=True)
-    password = fields.Char(string="Mật khẩu", required=True)
 
     def login_misa (self):
         misa_utils = self.env['misa.api.utils']
-        access_token = misa_utils._get_misa_token(self.username, self.password)
+        access_token = misa_utils._fetch_login_crm_token()
         if access_token:
             _logger.info("✅ Đăng nhập MISA thành công")
         else:
