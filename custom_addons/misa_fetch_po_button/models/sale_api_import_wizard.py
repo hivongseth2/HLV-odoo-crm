f80d7586e4dc1940c89339a3924544cdda652f5c
hivongseth2 <99702632+hivongseth2@users.noreply.github.com>
2025-07-01 10:28:58 +0700

    ok

--- custom_addons/misa_fetch_po_button/models/sale_api_import_wizard.py
+++ custom_addons/misa_fetch_po_button/models/sale_api_import_wizard.py
@@ -38,13 +38,13 @@ class SaleApiImportWizard(models.TransientModel):
 
         headers = {"Authorization": f"Bearer {token}", "Content-Type": "application/json"}
 
-        page = 1
+        page = 0
         page_size = 20
 
         start_datetime = datetime.combine(self.from_date, datetime.min.time())
         end_datetime = datetime.combine(self.to_date, datetime.max.time())
 
-        while page <= 50:
+        while page <= 10:
             params = {
                 "page": page,
                 "pageSize": page_size,
