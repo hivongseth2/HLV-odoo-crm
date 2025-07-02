cf5e3bde670eaeec200e2c18c6500bf692e7ed8b
hivongseth2 <99702632+hivongseth2@users.noreply.github.com>
2025-07-02 13:49:20 +0700

    ok

---
+++ custom_addons/hoanglongvu_delivery_note/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Hoang Long Vu Delivery Note",
+    "version": "1.0",
+    "depends": ["stock"],
+    "category": "Warehouse",
+    "description": "Print Biên Bản Giao Nhận & Logistic Tag trực tiếp từ Delivery Order",
+    "data": [
+        "report/report.xml"
+    ],
+    "installable": True,
+    "application": False
+}
