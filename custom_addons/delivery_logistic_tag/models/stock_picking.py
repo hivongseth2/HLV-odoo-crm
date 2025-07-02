b22dea6321aa0c840c0939298f4466f5242174d9
hivongseth2 <99702632+hivongseth2@users.noreply.github.com>
2025-07-02 13:32:40 +0700

    ok

--- custom_addons/delivery_logistic_tag/models/stock_picking.py
+++ custom_addons/delivery_logistic_tag/models/stock_picking.py
@@ -5,3 +5,6 @@ class StockPicking(models.Model):
 
     delivery_note_attachment = fields.Binary("Delivery Note", attachment=True)
     delivery_note_filename = fields.Char("Filename")
+    
+    def print_logistic_tag(self):
+        return self.env.ref('delivery_logistic_tag.report_logistic_tag').report_action(self)
