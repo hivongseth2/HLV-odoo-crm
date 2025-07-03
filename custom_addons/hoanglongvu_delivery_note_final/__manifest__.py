f8071dee9f4be4693ccd1ccd2fb484d34e4631b4
hivongseth2 <99702632+hivongseth2@users.noreply.github.com>
2025-07-03 11:36:28 +0700

    a

--- custom_addons/hoanglongvu_delivery_note_final/__manifest__.py
+++ custom_addons/hoanglongvu_delivery_note_final/__manifest__.py
@@ -3,11 +3,15 @@
     "version": "1.0",
     "depends": ["stock"],
     "category": "Warehouse",
-    "description": "Print Biên Bản Giao Nhận & Logistic Tag trực tiếp từ Delivery Order",
+    "description": "In Biên Bản Giao Nhận và Logistic Tag trực tiếp từ Delivery Order mới",
     "data": [
         "security/ir.model.access.csv",
         "report/report.xml",
-        "views/views.xml"
+        "views/views.xml",
+        "report/report_delivery_note.xml",
+        "report/report_logistic_tag.xml",
+
+
     ],
     "installable": True,
     "application": False
