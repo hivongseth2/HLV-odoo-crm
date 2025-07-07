10405a3caf5036b929215e5ec60f92713491ec85
hivongseth2 <99702632+hivongseth2@users.noreply.github.com>
2025-07-07 09:12:56 +0700

    ok

--- custom_addons/hoanglongvu_delivery_note/models/stock_move.py
+++ custom_addons/hoanglongvu_delivery_note/models/stock_move.py
@@ -1,13 +1,5 @@
 from odoo import models
 
-class StockPicking(models.Model):
-    _inherit = 'stock.picking'
-
-    def print_delivery_note(self):
-        return self.env.ref('hoanglongvu_delivery_note.action_report_delivery_note').report_action(self)
-
-
-
 class StockMove(models.Model):
     _inherit = 'stock.move'
 
