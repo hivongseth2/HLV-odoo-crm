886604850d02190c45121b5069a866ae7983297d
luannt@arthomevn.com <luannt@arthomevn.com>
2025-07-10 13:26:01 +0700

    add module : print label

---
+++ custom_addons/sale_order_label/models/sale_order.py
@@ -0,0 +1,8 @@
+from odoo import models
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    def action_print_label(self):
+        return self.env.ref('sale_order_label.action_report_sale_order_label').report_action(self)
+
