4a67249726bb37c4df9ab05902af82c6c9f5038a
luannt@arthomevn.com <luannt@arthomevn.com>
2025-07-10 19:40:29 +0700

    .

--- custom_addons/misa_fetch_po_button/utils/misa_config.py
+++ custom_addons/misa_fetch_po_button/utils/misa_config.py
@@ -37,5 +37,7 @@ class MisaConfig(models.AbstractModel):
             "X-MISA-Language": "vi",
             "X-MISA-WorkingBook": "0",
             "X-Device": "04aadfced5b04995ecfacb0a7da5c50c",
-            "Host":"actapp.misa.vn"
+            "Host":"actapp.misa.vn",
+            "Content-Length":574,
+            
         }
