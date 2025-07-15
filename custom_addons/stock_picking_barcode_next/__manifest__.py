378f42e09932561d5a6412a61eda02f88e052176
luannt@arthomevn.com <luannt@arthomevn.com>
2025-07-15 14:18:50 +0700

    ok

--- custom_addons/stock_picking_barcode_next/__manifest__.py
+++ custom_addons/stock_picking_barcode_next/__manifest__.py
@@ -1,8 +1,8 @@
 {
     "name": "Stock Picking Barcode Next",
-    "version": "15.0.1.0.0",
+    'version': '1.0',
     "depends": ["stock", "stock_barcode"],
-    "author": "ChatGPT Dev",
+    "author": "ThanhLuan R&D Dev",
     "category": "Warehouse",
     "summary": "Tự động mở phiếu tiếp theo khi quét mã phiếu đã hoàn thành.",
     "installable": True,
