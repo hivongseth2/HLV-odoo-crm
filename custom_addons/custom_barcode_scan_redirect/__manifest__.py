c05d1d3ddbb2ecf8095fd00d59d3d80ec274b3b1
luannt@arthomevn.com <luannt@arthomevn.com>
2025-07-15 17:06:34 +0700

    ok

--- custom_addons/custom_barcode_scan_redirect/__manifest__.py
+++ custom_addons/custom_barcode_scan_redirect/__manifest__.py
@@ -1,11 +1,15 @@
 {
     "name": "Custom Barcode Scan Redirect",
-    "version": "18.0.1.0.0",
+    "version": "0.0.1",
     "depends": ["stock", "stock_barcode"],
     "author": "ChatGPT Dev",
     "category": "Warehouse",
     "summary": "Redirect barcode scan to next picking if current is done, then load Odoo's native barcode view.",
     "installable": True,
     "application": False,
-    "license": "LGPL-3"
+    "license": "LGPL-3",
+    "data": ["views/scan_ui_template.xml"],
 }
+
+
+
