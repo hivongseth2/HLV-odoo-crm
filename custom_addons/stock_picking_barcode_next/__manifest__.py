7328dd7e0dace0875d681bcfd512111883159343
luannt@arthomevn.com <luannt@arthomevn.com>
2025-07-18 13:18:59 +0700

    oik

--- custom_addons/stock_picking_barcode_next/__manifest__.py
+++ custom_addons/stock_picking_barcode_next/__manifest__.py
@@ -12,7 +12,7 @@
     'auto_install': False,
     'assets': {
         'web.assets_backend': [
-            'stock_barcode_auto_next/static/src/js/barcode_picking_override.js',
+            'stock_picking_barcode_next/static/src/js/barcode_picking_override.js',
         ],
     },
 }
