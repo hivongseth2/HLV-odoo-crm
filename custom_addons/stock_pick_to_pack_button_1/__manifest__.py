1c3e40fc32d8149a5f326214b9f5958404beaff8
luannt@arthomevn.com <luannt@arthomevn.com>
2025-07-23 15:34:43 +0700

    .

--- custom_addons/stock_pick_to_pack_button_1/__manifest__.py
+++ custom_addons/stock_pick_to_pack_button_1/__manifest__.py
@@ -4,14 +4,13 @@
     "license": "LGPL-3",
     "category": "Inventory",
     "depends": ["stock_barcode",'web', 'stock'],
-    "data": [
-        "views/assets.xml"
-    ],
+
     'assets': {
         'web.assets_backend': [
-            'stock_pick_to_pack_button_1/static/src/js/redirect_to_pack.js',
+            '/stock_pick_to_pack_button_1/static/src/js/redirect_to_pack.js',
         ],
     },
+
 
     "installable": True,
     "application": False
