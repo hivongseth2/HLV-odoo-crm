bc94d973f9af849add8e97876f684765ec53d565
luannt@arthomevn.com <luannt@arthomevn.com>
2025-07-23 15:11:31 +0700

    .

--- custom_addons/stock_pick_to_pack_button/static/src/js/redirect_to_pack.js
+++ custom_addons/stock_pick_to_pack_button/static/src/js/redirect_to_pack.js
@@ -1,33 +1,28 @@
 /** @odoo-module **/
 
 import { registry } from "@web/core/registry";
-import { BarcodeMainComponent } from "@stock_barcode/components/barcode_main";
 
-const patchBarcode = {
-    async setup() {
-        await this._super(...arguments);
-        if (this.env.services.action.current?.res_model === 'stock.picking') {
-            const picking = this.env.services.action.current?.context?.barcode_picking_data;
-            if (picking?.state === 'done' && picking?.origin) {
-                const pack = await this.orm.searchRead('stock.picking', [
-                    ['origin', '=', picking.name],
-                    ['state', 'not in', ['done', 'cancel']],
-                    ['picking_type_id.code', '=', 'outgoing'],
-                ], ['id'], { limit: 1 });
+registry.category("barcode_handlers").add("redirect_to_pack_if_pick_done", {
+    async onBarcodeScanned({ env, barcode }) {
+        const result = await env.services.orm.call("stock.picking", "search_read", [
+            [["name", "=", barcode]],
+            ["id", "name", "origin", "state", "picking_type_id"]
+        ]);
 
-                if (pack.length) {
-                    this.env.services.action.doAction({
-                        type: 'ir.actions.client',
-                        tag: 'barcode_picking_client_action',
-                        params: { barcode_picking_id: pack[0].id },
-                    });
-                }
+        const picking = result[0];
+        if (picking?.state === 'done') {
+            const pack = await env.services.orm.call("stock.picking", "search_read", [
+                [["origin", "=", picking.name], ["state", "!=", "done"]],
+                ["id"]
+            ], { limit: 1 });
+
+            if (pack.length) {
+                env.services.action.doAction({
+                    type: "ir.actions.client",
+                    tag: "barcode_picking_client_action",
+                    params: { barcode_picking_id: pack[0].id },
+                });
             }
         }
     }
-};
-
-registry.category("actions").add("barcode_picking_client_action", {
-    ...BarcodeMainComponent,
-    ...patchBarcode,
 });
