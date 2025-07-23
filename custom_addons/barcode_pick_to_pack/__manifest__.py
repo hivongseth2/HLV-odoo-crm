f6756c05964af5e03600cac1df6e1c46a50089c7
luannt@arthomevn.com <luannt@arthomevn.com>
2025-07-23 13:35:10 +0700

    idk

--- custom_addons/barcode_pick_to_pack/__manifest__.py
+++ custom_addons/barcode_pick_to_pack/__manifest__.py
@@ -1,10 +1,13 @@
 {
-    "name": "Redirect Pick Done to Pack",
+    "name": "Barcode Pick to Pack (JS Override)",
     "version": "0.1",
+    "category": "Stock",
     "depends": ["stock_barcode"],
-    "data": [
-        "data/barcode_rule.xml"
-    ],
+    "assets": {
+        "stock_barcode.assets": [
+            "barcode_pick_to_pack_js/static/src/js/barcode_override.js",
+        ]
+    },
     "installable": True,
     "application": False,
 }
