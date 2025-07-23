601a0ae422ed637156d0a563134731bf72f0b579
luannt@arthomevn.com <luannt@arthomevn.com>
2025-07-23 13:20:16 +0700

    test

--- custom_addons/barcode_pick_to_pack/__manifest__.py
+++ custom_addons/barcode_pick_to_pack/__manifest__.py
@@ -1,11 +1,10 @@
 {
-    "name": "Barcode Pick Done Redirect to Pack",
+    "name": "Redirect Pick Done to Pack",
     "version": "0.1",
-    "category": "Inventory",
-    "depends": ["stock", "stock_barcode"],
-    "author": "ChatGPT x anh yêu",
-    "summary": "Redirect to pack when scanning a done pick using barcode.rule.",
-    "data": ["data/barcode_rule.xml"],
+    "depends": ["stock_barcode"],
+    "data": [
+        "data/barcode_rule.xml"
+    ],
     "installable": True,
-    "auto_install": False
+    "application": False,
 }
