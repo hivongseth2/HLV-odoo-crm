29d66f50d6363fcda8791e0bc7f079d48d70e901
luannt@arthomevn.com <luannt@arthomevn.com>
2025-07-24 08:09:33 +0700

    fix

--- custom_addons/custom_barcode_scan_redirect/__manifest__.py
+++ custom_addons/custom_barcode_scan_redirect/__manifest__.py
@@ -1,20 +1,22 @@
+
 {
     "name": "Custom Barcode Scan Redirect",
     "version": "1.0",
     "depends": ["stock", "stock_barcode"],
     "author": "Anh Yêu 123❤️",
     "category": "Warehouse",
-    "summary": "Scan barcode and redirect to next picking",
+    "summary": "Scan barcode and redirect to next picking or show products",
     "data": [
         "views/menu.xml",
-        "views/scan_ui_template.xml"
+        "views/scan_ui_template.xml",
+        "views/pack_products_template.xml"
     ],
     "installable": True,
     "application": False,
     "license": "LGPL-3",
-    'assets': {
-    'web.assets_frontend': [
-        'custom_barcode_scan_redirect/static/src/js/scan_ui.js',
-    ]
-}
+    "assets": {
+        "web.assets_frontend": [
+            "custom_barcode_scan_redirect/static/src/js/scan_ui.js"
+        ]
+    }
 }
