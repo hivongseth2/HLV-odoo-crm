62a1656d788874e40e0f62f0cdad3b880c3efcdd
luannt@arthomevn.com <luannt@arthomevn.com>
2025-07-24 14:42:37 +0700

    fix me mmmmmmmmm

--- custom_addons/custom_barcode_scan_redirect/controllers/main.py
+++ custom_addons/custom_barcode_scan_redirect/controllers/main.py
@@ -154,13 +154,11 @@ class CustomBarcodeScanController(http.Controller):
                         add_qty = min(delta, remain_qty)
                         new_qty = ml.qty_done + add_qty
                         ml.write({'qty_done': new_qty})
-                        ml.invalidate_cache(['qty_done'])
-                        ml = ml.sudo().browse(ml.id)
+                        ml = ml.sudo().browse(ml.id)  # Reload lại để lấy giá trị mới
 
                     elif delta < 0 and ml.qty_done > 0:
                         new_qty = max(0, ml.qty_done + delta)
                         ml.write({'qty_done': new_qty})
-                        ml.invalidate_cache(['qty_done'])
                         ml = ml.sudo().browse(ml.id)
 
 
