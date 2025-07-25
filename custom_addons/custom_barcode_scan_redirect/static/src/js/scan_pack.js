607062eab52209d5fbec28239dcad463e05ab1fe
luannt@arthomevn.com <luannt@arthomevn.com>
2025-07-25 13:22:08 +0700

    chuyen logic tim line id ve FE

--- custom_addons/custom_barcode_scan_redirect/static/src/js/scan_pack.js
+++ custom_addons/custom_barcode_scan_redirect/static/src/js/scan_pack.js
@@ -15,8 +15,26 @@ document.addEventListener("DOMContentLoaded", function () {
   function playError() {
     new Audio("/custom_barcode_scan_redirect/static/src/sound/error.mp3").play();
   }
+  function findLineToUpdate(barcode) {
+    const elements = [...document.querySelectorAll(`[data-barcode="${barcode}"]`)];
+    for (const el of elements) {
+      const doneEl = el.querySelector(".done");
+      const requiredEl = el.querySelectorAll("span")[1];
+
+      const done = parseFloat(doneEl?.innerText || 0);
+      const required = parseFloat(requiredEl?.innerText || 0);
+
+      if (done < required) {
+        return el.dataset.lineId;
+      }
+    }
+    return null; // tất cả đã đủ
+  }
 
   function updateQty(barcode, delta = 1, lineId = null) {
+    if (!lineId) {
+      lineId = findLineToUpdate(barcode);
+    } 
     fetch("/pack_scan/scan_item", {
       method: "POST",
       headers: {
