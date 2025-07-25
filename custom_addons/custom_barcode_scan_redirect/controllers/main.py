d68cabf782532eef674f9f6afbe34750aaee114c
luannt@arthomevn.com <luannt@arthomevn.com>
2025-07-25 15:39:16 +0700

    ok

--- custom_addons/custom_barcode_scan_redirect/controllers/main.py
+++ custom_addons/custom_barcode_scan_redirect/controllers/main.py
@@ -175,7 +175,7 @@ class CustomBarcodeScanController(http.Controller):
                         updated_lines.append({
                             "line_id": ml.id,
                             "product": move.product_id.display_name,
-                            "done_qty": total_done - current_qty + new_qty,
+                            "done_qty": total_done  + delta ,
                             "required_qty": move.product_uom_qty
                         })
                         break
