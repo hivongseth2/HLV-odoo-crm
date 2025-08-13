e193f37721e0fba4252992dde3946219989ea334
luannt@arthomevn.com <luannt@arthomevn.com>
2025-08-13 18:21:07 +0700

    .

--- custom_addons/stock_barcode_autosave_o18_server/__manifest__.py
+++ custom_addons/stock_barcode_autosave_o18_server/__manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": "Barcode Autosave Odoo 18 (Server-assisted)",
     "summary": "Persist qty_done to server immediately on scan/change using a helper method on stock.picking.",
-    "version": "18.0.1",
+    "version": "18.0.1.0.0",
     "license": "LGPL-3",
     "depends": ["stock_barcode", "stock"],
     "data": [],
