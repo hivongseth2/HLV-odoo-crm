fb3f36f816d47899628524d71bcfc016680d1416
luannt@arthomevn.com <luannt@arthomevn.com>
2025-08-13 17:09:02 +0700

    fix

--- custom_addons/stock_barcode_autosave_o18_fix/__manifest__.py
+++ custom_addons/stock_barcode_autosave_o18_fix/__manifest__.py
@@ -1,14 +1,14 @@
 
 {
-    "name": "Barcode Autosave (Odoo 18)",
+    "name": "Barcode Autosave Odoo 18 (Fix)",
     "summary": "Autosave qty_done on scan/change for Barcode app without touching core.",
-    "version": "18.1",
+    "version": "18.0.1.0.1",
     "license": "LGPL-3",
-    "installable": True,
+    "installable": true,
     "depends": ["stock_barcode"],
     "assets": {
         "web.assets_backend": [
-            "stock_barcode_autosave/static/src/js/autosave_barcode.js"
+            "stock_barcode_autosave_o18_fix/static/src/js/autosave_barcode.js"
         ]
     }
 }
