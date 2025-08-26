62e28fbea33ba89fa6a37e5d01ee28ac00fe3b32
luannt@arthomevn.com <luannt@arthomevn.com>
2025-08-26 23:51:56 +0700

    orverride logic

--- custom_addons/stock_orderpoint_ignore_forecast/models/stock_orderpoint.py
+++ custom_addons/stock_orderpoint_ignore_forecast/models/stock_orderpoint.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from odoo import fields, models
+from math import ceil
+from odoo import api, fields, models
 from odoo.tools.float_utils import float_round
 
 class StockWarehouseOrderpoint(models.Model):
@@ -11,24 +12,44 @@ class StockWarehouseOrderpoint(models.Model):
         default=True,
     )
 
-    def _compute_ignore_forecast_qty(self, product):
-        """Bù đến min dựa trên tồn thực tế, làm tròn theo UoM của sản phẩm."""
-        on_hand = product.qty_available
-        need = max(0.0, self.product_min_qty - on_hand)
-        # Trên Odoo 16, orderpoint hiển thị product_uom_name, dùng rounding của product.uom_id
+    def _compute_ignore_forecast_qty(self, product, min_qty, on_hand, qty_multiple):
+        """Bù đến min dựa trên tồn thực tế + tôn trọng bội số và UoM."""
+        need = max(0.0, min_qty - on_hand)
+        # bội số đặt hàng (nếu có) – làm tròn lên theo bội số
+        if qty_multiple:
+            need = ceil(need / qty_multiple) * qty_multiple
+        # làm tròn theo UoM của sản phẩm
         need = float_round(need, precision_rounding=product.uom_id.rounding)
         return need
 
-    # Trường hợp scheduler gọi _quantity_to_order(...)
+    # ① Dùng cho scheduler / replenishment
     def _quantity_to_order(self, product, qty_available, qty_forecast, **kwargs):
         self.ensure_one()
         if self.ignore_forecast:
-            return self._compute_ignore_forecast_qty(product)
+            return self._compute_ignore_forecast_qty(
+                product=product,
+                min_qty=self.product_min_qty,
+                on_hand=product.qty_available,
+                qty_multiple=self.qty_multiple,
+            )
         return super()._quantity_to_order(product, qty_available, qty_forecast, **kwargs)
 
-    # Trường hợp build khác gọi _get_orderpoint_procurement_qty(...)
-    def _get_orderpoint_procurement_qty(self):
-        self.ensure_one()
-        if self.ignore_forecast:
-            return self._compute_ignore_forecast_qty(self.product_id)
-        return super()._get_orderpoint_procurement_qty()
+    # ② Dùng cho hiển thị cột "Cần đặt hàng" (list) & nút Order
+    @api.depends(
+        'ignore_forecast',       # thêm để đổi checkbox là tự recompute
+        'product_min_qty', 'product_max_qty',
+        'qty_on_hand', 'qty_forecast',
+        'qty_multiple', 'trigger'
+    )
+    def _compute_qty_to_order(self):
+        # tính mặc định trước (để trường hợp không tick thì theo Odoo)
+        super()._compute_qty_to_order()
+        for op in self:
+            # chỉ can thiệp khi auto (để không ghi đè số nhập tay)
+            if op.ignore_forecast and op.trigger == 'auto':
+                op.qty_to_order = op._compute_ignore_forecast_qty(
+                    product=op.product_id,
+                    min_qty=op.product_min_qty,
+                    on_hand=op.qty_on_hand,       # số thực tế đang hiển thị ở list
+                    qty_multiple=op.qty_multiple,
+                )
