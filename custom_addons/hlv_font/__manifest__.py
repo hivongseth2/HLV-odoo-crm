6d7e054b4cd057b8d078409b3498594c90c1e915
luannt@arthomevn.com <luannt@arthomevn.com>
2025-09-11 11:41:02 +0700

    ok

---
+++ custom_addons/hlv_font/__manifest__.py
@@ -0,0 +1,17 @@
+# hlv_font/__manifest__.py
+{
+    "name": "HLV Global PDF Font",
+    "summary": "Áp font Times New Roman (hoặc bất kỳ font TTF) cho toàn bộ báo cáo PDF",
+    "version": "18.0.1.0.0",
+    "author": "HLV",
+    "website": "",
+    "license": "LGPL-3",
+    "category": "Reporting",
+    "depends": ["web"],
+    "data": [
+        "views/report_font.xml",
+    ],
+    "assets": {},
+    "installable": True,
+    "application": False,
+}
