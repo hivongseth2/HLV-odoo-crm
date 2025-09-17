4df328997a253ec238a767c11602e93cac2a0b42
luannt@arthomevn.com <luannt@arthomevn.com>
2025-09-17 14:19:33 +0700

    ok

--- custom_addons/hlv_pack_sequence/hlv_pack_sequence/models/stock_package_level.py
+++ custom_addons/hlv_pack_sequence/hlv_pack_sequence/models/stock_package_level.py
@@ -1,20 +1,28 @@
-# -*- coding: utf-8 -*-
 from odoo import api, fields, models
 
 class StockQuantPackage(models.Model):
     _inherit = "stock.quant.package"
 
-    pack_sequence = fields.Integer(string="Package Sequence", compute="_compute_pack_seq", store=True)
-    pack_total = fields.Integer(string="Total Packages", compute="_compute_pack_seq", store=True)
+    # Không store để khỏi cần depends, tính lúc in label là đủ
+    pack_sequence = fields.Integer(string="Package Sequence", compute="_compute_pack_seq")
+    pack_total = fields.Integer(string="Total Packages", compute="_compute_pack_seq")
 
-    @api.depends('picking_ids', 'picking_ids.package_ids')
     def _compute_pack_seq(self):
+        MoveLine = self.env['stock.move.line']
         for pack in self:
-            if pack.picking_ids:
-                # lấy phiếu đầu tiên (đa số 1 kiện = 1 picking)
-                picking = pack.picking_ids[0]
-                all_packs = picking.package_ids.sorted("id")
-                total = len(all_packs)
-                for idx, p in enumerate(all_packs, start=1):
-                    p.pack_sequence = idx
-                    p.pack_total = total
+            # Tìm 1 picking chứa kiện này qua move lines (result_package_id)
+            ml = MoveLine.search([('result_package_id', '=', pack.id)], limit=1)
+            picking = ml.picking_id if ml else False
+            if not picking:
+                pack.pack_sequence = 0
+                pack.pack_total = 0
+                continue
+
+            # Lấy tất cả kiện của picking (qua move lines), loại trùng và sắp theo id
+            pack_ids = list({ml.result_package_id.id for ml in picking.move_line_ids if ml.result_package_id})
+            pack_ids.sort()
+            total = len(pack_ids)
+            seq = pack_ids.index(pack.id) + 1 if pack.id in pack_ids else 0
+
+            pack.pack_total = total
+            pack.pack_sequence = seq
