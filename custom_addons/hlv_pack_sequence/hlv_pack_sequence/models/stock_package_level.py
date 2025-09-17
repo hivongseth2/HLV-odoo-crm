dc3fc052725ba349ac74eb13d413dbdf0b357ae0
luannt@arthomevn.com <luannt@arthomevn.com>
2025-09-17 13:27:37 +0700

    ok

--- custom_addons/hlv_pack_sequence/hlv_pack_sequence/models/stock_package_level.py
+++ custom_addons/hlv_pack_sequence/hlv_pack_sequence/models/stock_package_level.py
@@ -1,22 +1,20 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models
 
-class StockPackageLevel(models.Model):
-    _inherit = "stock.package.level"
+class StockQuantPackage(models.Model):
+    _inherit = "stock.quant.package"
 
     pack_sequence = fields.Integer(string="Package Sequence", compute="_compute_pack_seq", store=True)
     pack_total = fields.Integer(string="Total Packages", compute="_compute_pack_seq", store=True)
 
-    @api.depends('picking_id', 'picking_id.package_level_ids')
+    @api.depends('picking_ids', 'picking_ids.package_ids')
     def _compute_pack_seq(self):
-        # Compute numbering X/Y within each picking
-        picking_map = {}
-        for lvl in self:
-            if lvl.picking_id:
-                picking_map.setdefault(lvl.picking_id.id, lvl.picking_id)
-        for picking in picking_map.values():
-            levels = picking.package_level_ids.sorted('id')
-            total = len(levels)
-            for idx, lvl in enumerate(levels, start=1):
-                lvl.pack_sequence = idx
-                lvl.pack_total = total
+        for pack in self:
+            if pack.picking_ids:
+                # lấy phiếu đầu tiên (đa số 1 kiện = 1 picking)
+                picking = pack.picking_ids[0]
+                all_packs = picking.package_ids.sorted("id")
+                total = len(all_packs)
+                for idx, p in enumerate(all_packs, start=1):
+                    p.pack_sequence = idx
+                    p.pack_total = total
