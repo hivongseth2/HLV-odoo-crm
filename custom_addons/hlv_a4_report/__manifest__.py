a98d14bf81debafa5717f1ac38bfed2101f819d3
TuPham <atu3012@gmail.com>
2025-09-24 12:01:01 +0700

    fix font again

--- custom_addons/hlv_a4_report/__manifest__.py
+++ custom_addons/hlv_a4_report/__manifest__.py
@@ -20,13 +20,15 @@
 
     # always loaded
     'data': [
-        'static/fonts/*',
         'report/paperformat.xml',
         'report/bbgn_a4_khongngay.xml',
     ],
     "assets": {
         "web.report_assets_common": [
-            "hlv_a4_report/static/css/report.css",
+            'hlv_a4_report/static/fonts/times-new-roman.ttf',
+            'hlv_a4_report/static/fonts/times-new-roman-bold.ttf',
+            'hlv_a4_report/static/fonts/times-new-roman-italic.ttf',
+            'hlv_a4_report/static/fonts/times-new-roman-bolditalic.ttf',
         ],
     },
 }
