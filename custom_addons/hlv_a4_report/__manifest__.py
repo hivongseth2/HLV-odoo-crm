a66c270e42bb654bf1f3fca0f428d75de8a78d67
TuPham <atu3012@gmail.com>
2025-09-26 09:07:35 +0700

    fix: fix form to chose date

--- custom_addons/hlv_a4_report/__manifest__.py
+++ custom_addons/hlv_a4_report/__manifest__.py
@@ -27,5 +27,6 @@
         'report/pxbh_khachle_khonghd.xml',
         'report/bbbg_a4_co_po.xml',
         'views/bbgn_date_wizard_views.xml',
+        'report/print_proxy.xml',
     ],
 }
--- custom_addons/hlv_a4_report/models/bbgn_date_wizard.py
+++ custom_addons/hlv_a4_report/models/bbgn_date_wizard.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields, api, _
-from datetime import date
+from odoo import models, fields
 
 class BbgnDateWizard(models.TransientModel):
     _name = 'bbgn.date.wizard'
@@ -11,13 +10,7 @@ class BbgnDateWizard(models.TransientModel):
 
     def action_print(self):
         self.ensure_one()
-        # Định dạng dd/mm/YYYY để hiển thị đúng yêu cầu
         order_date_str = self.order_date.strftime('%d/%m/%Y') if self.order_date else ''
-        # Gọi report và truyền ngày qua context
-        action = self.env.ref('hlv_a4_report.bbgn_a4_khong_gia').report_action(
-            self.picking_id,
-            config=False
-        )
-        # Ép context kèm ngày đã chọn
+        action = self.env.ref('hlv_a4_report.bbgn_a4_khong_gia').report_action(self.picking_id, config=False)
         action['context'] = dict(self.env.context, bbgn_order_date=order_date_str)
         return action
