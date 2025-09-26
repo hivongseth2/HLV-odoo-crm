a7d38856db32f705f276e3b62281fc4da0d6e10a
TuPham <atu3012@gmail.com>
2025-09-26 09:31:04 +0700

    fix manifest

--- custom_addons/hlv_a4_report/__manifest__.py
+++ custom_addons/hlv_a4_report/__manifest__.py
@@ -12,7 +12,7 @@
     "author": "Your Company",
     "website": "http://www.yourcompany.com",
     "category": "Inventory",
-    "version": "18.0.1.0.1",   
+    "version": "18.0.1.0.0",   
     "license": "LGPL-3",
 
     # any module necessary for this one to work correctly
