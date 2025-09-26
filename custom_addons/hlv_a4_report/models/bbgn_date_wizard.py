b8266ec5d2520931bf3d47ce996590a30bc921f9
TuPham <atu3012@gmail.com>
2025-09-26 09:11:07 +0700

    fix: fix get date wizard

--- custom_addons/hlv_a4_report/models/bbgn_date_wizard.py
+++ custom_addons/hlv_a4_report/models/bbgn_date_wizard.py
@@ -11,6 +11,10 @@ class BbgnDateWizard(models.TransientModel):
     def action_print(self):
         self.ensure_one()
         order_date_str = self.order_date.strftime('%d/%m/%Y') if self.order_date else ''
-        action = self.env.ref('hlv_a4_report.bbgn_a4_khong_gia').report_action(self.picking_id, config=False)
+        action = self.env.ref('hlv_a4_report.bbgn_a4_khong_gia').sudo().report_action(
+            self.picking_id,
+            config=False
+        )
         action['context'] = dict(self.env.context, bbgn_order_date=order_date_str)
         return action
+
