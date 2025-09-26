4d18331dda2824abeb13a425270ae86997bfc485
TuPham <atu3012@gmail.com>
2025-09-26 09:26:36 +0700

    fix: fix lỗi không lấy được dữ liệu

--- custom_addons/hlv_a4_report/models/bbgn_date_wizard.py
+++ custom_addons/hlv_a4_report/models/bbgn_date_wizard.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 from odoo import models, fields
 
 class BbgnDateWizard(models.TransientModel):
@@ -10,11 +9,15 @@ class BbgnDateWizard(models.TransientModel):
 
     def action_print(self):
         self.ensure_one()
-        order_date_str = self.order_date.strftime('%d/%m/%Y') if self.order_date else ''
+        # Date field đôi khi là string 'YYYY-MM-DD', chuyển sang date rồi format an toàn:
+        dt = fields.Date.to_date(self.order_date) if self.order_date else None
+        order_date_str = dt.strftime('%d/%m/%Y') if dt else ''
+
         action = self.env.ref('hlv_a4_report.bbgn_a4_khong_gia').sudo().report_action(
             self.picking_id,
+            data={'bbgn_order_date': order_date_str},   # ƯU TIÊN đọc từ data trong QWeb
             config=False
         )
+        # Dự phòng: nhét vào context nữa (nếu môi trường không bơm 'data' vào template)
         action['context'] = dict(self.env.context, bbgn_order_date=order_date_str)
         return action
-
