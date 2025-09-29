88d72c94fe2e28dd4ab806cf37eecec1852c44c8
TuPham <atu3012@gmail.com>
2025-09-29 15:27:34 +0700

    fix: thay đổi file chứa logic

--- custom_addons/misa_fetch_po_button/models/misa_po_fetch.py
+++ custom_addons/misa_fetch_po_button/models/misa_po_fetch.py
@@ -172,10 +172,6 @@ class MisaPOFetch(models.TransientModel):
                 receive_date_str = po.get("receive_date") or po.get("refdate")
                 planned_naive_utc = _to_naive_utc(receive_date_str)
 
-                delivery_status = po.get("DeliveryStatusID", "0")
-                if delivery_status is not None and str(delivery_status).strip() == "2":
-                    _logger.info("⏭️  Bỏ qua PO %s (refid=%s) vì Đơn hàng đã giao", refno, refid)
-                    continue
 
                 partner = odoo_utils._get_or_create_partner(supplier_name)
 
