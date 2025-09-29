6d1f5b82b03a8aebcb8e0940c07180f9c8fb1977
TuPham <atu3012@gmail.com>
2025-09-29 14:16:48 +0700

    fix: test lại logic lấy đơn hàng

--- custom_addons/misa_fetch_po_button/models/misa_po_fetch.py
+++ custom_addons/misa_fetch_po_button/models/misa_po_fetch.py
@@ -173,8 +173,8 @@ class MisaPOFetch(models.TransientModel):
                 planned_naive_utc = _to_naive_utc(receive_date_str)
 
                 delivery_status = po.get("DeliveryStatusID")
-                if str(delivery_status).strip() != "1":
-                    _logger.info("⏭️  Bỏ qua PO %s (refid=%s) vì DeliveryStatusID != 1", refno, refid)
+                if str(delivery_status).strip() == "2":
+                    _logger.info("⏭️  Bỏ qua PO %s (refid=%s) vì Đơn hàng đã giao", refno, refid)
                     continue
 
                 partner = odoo_utils._get_or_create_partner(supplier_name)
