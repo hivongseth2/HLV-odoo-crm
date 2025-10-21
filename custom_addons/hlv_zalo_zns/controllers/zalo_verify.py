581f097ada3775f65ceddd81e2b6d367591e1cdb
luannt@arthomevn.com <luannt@arthomevn.com>
2025-10-21 10:27:26 +0700

    fix zalo verifier

--- custom_addons/hlv_zalo_zns/controllers/zalo_verify.py
+++ custom_addons/hlv_zalo_zns/controllers/zalo_verify.py
@@ -1,15 +1,18 @@
 # controllers/zalo_verify.py
 from odoo import http
 
-# TH 1: route cố định đúng tên file Zalo yêu cầu (đơn giản nhất)
-class ZaloVerifyFixed(http.Controller):
-    @http.route(
-        ['/zalo_verifierKlhW2wRGLp15rFikZ_v-4tVer0kRdWDKCpCt.html'],
-        type='http', auth='public', csrf=False, website=True
-    )
-    def zalo_verify_fixed(self, **kwargs):
-        # Theo tài liệu, nội dung thường chỉ cần là 1 HTML hợp lệ (có thể rỗng body)
-        return """
+class ZaloVerifyController(http.Controller):
+    @http.route('/zalo_verifierKlhW2wRGLp15rFikZ_v-4tVer0kRdWDKCpCt.html', type='http', auth='public', csrf=False)
+    def zalo_verify(self, **kw):
+        html = """
 <!DOCTYPE html>
-<html><head><meta charset="utf-8"></head><body>ok</body></html>
-""".strip()
+<html lang="en">
+<head>
+    <meta property="zalo-platform-site-verification" content="KlhW2wRGLp15rFikZ_v-4tVer0kRdWDKCpCt" />
+</head>
+<body>
+There Is No Limit To What You Can Accomplish Using Zalo!
+</body>
+</html>
+"""
+        return html
