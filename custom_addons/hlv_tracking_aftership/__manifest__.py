400981d50a42b36f9acd0552edb3dfcfffe68f16
luannt@arthomevn.com <luannt@arthomevn.com>
2025-10-23 16:35:48 +0700

    fix

--- custom_addons/hlv_tracking_aftership/__manifest__.py
+++ custom_addons/hlv_tracking_aftership/__manifest__.py
@@ -1,10 +1,10 @@
 {
     "name": "HLV Tracking AfterShip",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.0.1",
     "summary": "Track J&T Express VN via AfterShip (slug: jtexpress-vn)",
     "author": "Hoang Long Vu + ChatGPT",
     "license": "LGPL-3",
-    "depends": ["stock"],
+    "depends": ["stock","website"],
     "data": [
         "views/stock_picking_buttons.xml",
         "data/ir_cron.xml",
