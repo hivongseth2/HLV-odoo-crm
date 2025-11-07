603f392ffcdab1285332ffc385bfbe6a603f1579
luannt@arthomevn.com <luannt@arthomevn.com>
2025-11-07 09:41:38 +0700

    add xml to view

--- custom_addons/hlv_barcode_stock_qty_v3/__manifest__.py
+++ custom_addons/hlv_barcode_stock_qty_v3/__manifest__.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'HLV Barcode Stock Quantity (v3 inline)',
-    'version': '1.2.3',
+    'version': '1.2.4',  # tăng version để Odoo rebuild assets
     'summary': 'Hiển thị tồn kho khi quét barcode (inline ngay sau 0/x Cái)',
     'author': 'HLV - Thanh Luan',
     'category': 'Inventory/Barcode',
     'depends': ['stock', 'stock_barcode'],
-    'data': [],
+    'data': [
+        'views/assets.xml',   # <-- QUAN TRỌNG
+    ],
     'assets': {
+        # có thể giữ, nhưng trọng tâm là assets.xml:
         'stock_barcode.assets_backend': [
             '/hlv_barcode_stock_qty_v3/static/src/js/barcode_inline_qty.js',
             '/hlv_barcode_stock_qty_v3/static/src/css/inline.css',
@@ -15,8 +18,7 @@
         'web.assets_backend': [
             '/hlv_barcode_stock_qty_v3/static/src/js/barcode_inline_qty.js',
             '/hlv_barcode_stock_qty_v3/static/src/css/inline.css',
-        ]
-      
+        ],
     },
     'installable': True,
     'application': False,
