e76e68bb5033af5ce1bdd344bdd0c30b7b4690e8
luannt@arthomevn.com <luannt@arthomevn.com>
2025-11-07 10:24:07 +0700

    remove assets

--- custom_addons/hlv_barcode_stock_qty_v3/__manifest__.py
+++ custom_addons/hlv_barcode_stock_qty_v3/__manifest__.py
@@ -5,20 +5,25 @@
     'author': 'HLV - Thanh Luan',
     'category': 'Inventory/Barcode',
     'depends': ['stock', 'stock_barcode'],
-    'data': [
-        'views/assets.xml',   # <-- QUAN TRỌNG
-    ],
+
     'assets': {
-        # có thể giữ, nhưng trọng tâm là assets.xml:
+        # 1) Những bundle có thể được dùng bởi Barcode app tùy biến thể/version
         'stock_barcode.assets_backend': [
             '/hlv_barcode_stock_qty_v3/static/src/js/barcode_inline_qty.js',
             '/hlv_barcode_stock_qty_v3/static/src/css/inline.css',
         ],
+        'stock_barcode.assets_common': [
+            '/hlv_barcode_stock_qty_v3/static/src/js/barcode_inline_qty.js',
+            '/hlv_barcode_stock_qty_v3/static/src/css/inline.css',
+        ],
+
+        # 2) Luôn luôn nạp ở backend chung (phương án cứu cánh)
         'web.assets_backend': [
             '/hlv_barcode_stock_qty_v3/static/src/js/barcode_inline_qty.js',
             '/hlv_barcode_stock_qty_v3/static/src/css/inline.css',
         ],
     },
+
     'installable': True,
     'application': False,
     'license': 'LGPL-3',
