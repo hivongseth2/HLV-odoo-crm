9c3d4b831421846c1481d692d85d42560c6078e3
luannt@arthomevn.com <luannt@arthomevn.com>
2025-11-17 15:25:22 +0700

    update token

--- custom_addons/hlv_zalo_zns/controllers/zalo_verify.py
+++ custom_addons/hlv_zalo_zns/controllers/zalo_verify.py
@@ -1,15 +1,54 @@
-# controllers/zalo_verify.py
+# # controllers/zalo_verify.py
+# from odoo import http
+
+# class ZaloVerifyController(http.Controller):
+#     @http.route('/zalo_verifierKlhW2wRGLp15rFikZ_v-4tVer0kRdWDKCpCt.html', type='http', auth='public', csrf=False)
+#     def zalo_verify(self, **kw):
+#         html = """
+# <!DOCTYPE html>
+# <html lang="en">
+# <head>
+#     <meta property="zalo-platform-site-verification" content="KlhW2wRGLp15rFikZ_v-4tVer0kRdWDKCpCt" />
+    
+# </head>
+# <body>
+# There Is No Limit To What You Can Accomplish Using Zalo!
+# </body>
+# </html>
+# """
+#         return html
+
+
+
 from odoo import http
+from odoo.http import request
 
 class ZaloVerifyController(http.Controller):
-    @http.route('/zalo_verifierKlhW2wRGLp15rFikZ_v-4tVer0kRdWDKCpCt.html', type='http', auth='public', csrf=False)
+
+    TOKENS = [
+        "KlhW2wRGLp15rFikZ_v-4tVer0kRdWDKCpCt",
+        "CuIw8EUG4oeaWgOA_Aey27sgfoUHt75wDpOv",
+        
+    ]
+
+    @http.route([
+        '/zalo_verifierKlhW2wRGLp15rFikZ_v-4tVer0kRdWDKCpCt.html',
+        '/zalo_verifierCuIw8EUG4oeaWgOA_Aey27sgfoUHt75wDpOv.html',
+    ], type='http', auth='public', csrf=False)
     def zalo_verify(self, **kw):
-        html = """
+        path = request.httprequest.path
+        token = ""
+        for code in self.TOKENS:
+            if code in path:
+                token = code
+                break
+
+        html = f"""
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta property="zalo-platform-site-verification" content="KlhW2wRGLp15rFikZ_v-4tVer0kRdWDKCpCt" />
-    <meta name="zalo-platform-site-verification" content="CuIw8EUG4oeaWgOA_Aey27sgfoUHt75wDpOv" />
+    <meta property="zalo-platform-site-verification"
+          content="{token}" />
 </head>
 <body>
 There Is No Limit To What You Can Accomplish Using Zalo!
