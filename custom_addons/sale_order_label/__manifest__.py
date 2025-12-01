5f8f960ac8b87d1e128bff727e9974385fd4e79e
TuPham <atu3012@gmail.com>
2025-12-01 11:40:54 +0700

    thêm in nhãn cho phiếu stock.picking

--- custom_addons/sale_order_label/__manifest__.py
+++ custom_addons/sale_order_label/__manifest__.py
@@ -2,13 +2,14 @@
     "name": "Sale Order Label",
     "version": "1.0",
     "category": "Sales",
-    "summary": "Print 40x30mm label for sale orders",
-    "depends": ["sale_management", "base", "web"],
+    "summary": "Print 40x30mm label for sale orders and stock pickings",
+    "depends": ["sale_management", "stock", "base", "web"],
     "data": [
         "report/sale_order_label_template.xml",
         "report/sale_order_label_report.xml",
-                "views/sale_order_view.xml"
-
+        "report/stock_picking_label_template.xml",
+        "report/stock_picking_label_report.xml",
+        "views/sale_order_view.xml",
     ],
     "installable": True,
     "application": False,
