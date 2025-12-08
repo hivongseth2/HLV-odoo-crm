2319d847cbfdc66aa4b0f1b6e77a4c775b547437
TuPham <atu3012@gmail.com>
2025-12-08 15:14:23 +0700

    fix

--- custom_addons/stock_warehouse_dashboard/static/src/js/stock_dashboard_field.js
+++ custom_addons/stock_warehouse_dashboard/static/src/js/stock_dashboard_field.js
@@ -9,7 +9,6 @@ export class StockDashboardField extends Component {
     setup() {
         this.orm = useService("orm");
         this.action = useService("action");
-        this.user = useService("user");
 
         const today = new Date();
         const dateStr = today.getFullYear() + "-" + String(today.getMonth() + 1).padStart(2, '0') + "-" + String(today.getDate()).padStart(2, '0');
