9124b2cf9882689c133536c812853889dd0a30b5
TuPham <atu3012@gmail.com>
2025-12-16 16:45:28 +0700

    tạo website cho api tìm kiếm chứng từ mua hàng

--- custom_addons/misa_fetch_po_button/__manifest__.py
+++ custom_addons/misa_fetch_po_button/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'MISA PO Fetch Button',
-    'version': '1.0',
-    'depends': ['base', 'stock', 'purchase', 'sale'],
+    'version': '1.1',
+    'depends': ['base', 'stock', 'purchase', 'sale', 'website'],
     'author': 'Luan',
     'category': 'Purchases',
     'description': 'Fetch PO from MISA and create in Odoo',
@@ -13,10 +13,16 @@
         'views/misa_combined_button_view.xml',
         'views/misa_return_button_view.xml',
         'views/sale_order_misa_sync.xml',
-        'views/product_view.xml'
+        'views/product_view.xml',
+        'views/misa_purchase_lookup.xml',
     ],
+    'assets': {
+        'web.assets_frontend': [
+            'misa_fetch_po_button/static/src/css/misa_lookup.css',
+        ],
+    },
     'python': [
-        'models/sale_api_import_wizard.py',  # Thêm file này
+        'models/sale_api_import_wizard.py',
     ],
     'installable': True,
     'auto_install': False,
