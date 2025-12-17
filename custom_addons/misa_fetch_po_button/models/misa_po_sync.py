514aa5c2ec754e214b82d83f43317ec8ba00583b
luannt@arthomevn.com <luannt@arthomevn.com>
2025-12-17 10:00:27 +0700

    fix

--- custom_addons/misa_fetch_po_button/models/misa_po_sync.py
+++ custom_addons/misa_fetch_po_button/models/misa_po_sync.py
@@ -610,7 +610,8 @@ class MisaPOSync(models.TransientModel):
                 "HIENDUC": "KHD/Tồn kho",
                 "HCM_SHOWROOM": "TSNSR/Stock",
                 "HLV":"HLV/Stock",
-                 "BẾN CAM": "KBC/Tồn kho",
+                "BẾN CAM": "KBC/Tồn kho",
+                 "BẾNCAM": "KBC/Tồn kho",
             }
 
         if stock_code not in stock_mapping:
