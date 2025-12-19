d329e3fce552e5f0b58abe7e472b6790deecc41e
TuPham <atu3012@gmail.com>
2025-12-19 16:52:14 +0700

    thử lại api search

--- custom_addons/misa_fetch_po_button/utils/misa_api_utils.py
+++ custom_addons/misa_fetch_po_button/utils/misa_api_utils.py
@@ -1198,7 +1198,7 @@ class MisaApiUtils(models.AbstractModel):
         if code:
             filters.append({
                 "Group": None, "Addition": 1, "InputType": 1, "IsFromFormula": True,
-                "Operator": 8, "Property": "ProductCode", "Text": code.strip(), "Value": code.strip()
+                "Operator": 7, "Property": "ProductCode", "Text": code.strip(), "Value": code.strip()
             })
         
         if len(filters) == 1:
