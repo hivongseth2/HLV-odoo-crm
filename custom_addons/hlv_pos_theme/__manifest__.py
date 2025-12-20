9ee3327382213341435466c117c0dd3657c07db3
TuPham <atu3012@gmail.com>
2025-12-20 08:42:00 +0700

    test custom danh mục

---
+++ custom_addons/hlv_pos_theme/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'HLV POS Theme',
+    'version': '18.0.1.0.0',
+    'category': 'Point of Sale',
+    'summary': 'Clean and minimalist theme for Odoo POS',
+    'description': """
+        This module customizes the POS interface to replace the default colorful categories 
+        with a cleaner, more professional look.
+    """,
+    'author': 'HLV',
+    'website': '',
+    'license': 'LGPL-3',
+    'depends': ['point_of_sale'],
+    'data': [],
+    'assets': {
+        'point_of_sale._assets_pos': [
+            'hlv_pos_theme/static/src/css/pos_theme.css',
+        ],
+    },
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
