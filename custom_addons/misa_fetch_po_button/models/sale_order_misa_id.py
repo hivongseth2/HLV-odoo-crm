b762147d9a5c0c78654abf66bc79207696317451
TuPham <atu3012@gmail.com>
2025-12-20 15:25:38 +0700

    lại

--- custom_addons/misa_fetch_po_button/models/sale_order_misa_id.py
+++ custom_addons/misa_fetch_po_button/models/sale_order_misa_id.py
@@ -5,6 +5,13 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     misa_id = fields.Char(string="MISA ID", copy=False, index=True)
+    x_studio_crm_elivery = fields.Boolean(
+        string="Vận chuyển CRM", 
+        default=False,
+        copy=False,
+        help="Khi tích chọn, phiếu xuất kho sẽ tự động tạo tuyến vận chuyển trên MISA CRM"
+    )
+
 
 
 class SaleOrderLine(models.Model):
