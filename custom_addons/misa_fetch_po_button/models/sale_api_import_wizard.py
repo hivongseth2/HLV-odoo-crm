09c6ddb4beefa4c6087eb8cb06f410f7ad22b9d9
TuPham <atu3012@gmail.com>
2025-12-20 09:05:42 +0700

    ok

--- custom_addons/misa_fetch_po_button/models/sale_api_import_wizard.py
+++ custom_addons/misa_fetch_po_button/models/sale_api_import_wizard.py
@@ -790,7 +790,9 @@ class SaleApiImportWizard(models.TransientModel):
             "KHÁCH HÀNG KHÔNG CUNG CẤP THÔNG TIN_TIKTOK",
             "KHÁCH HÀNG KHÔNG CUNG CẤP THÔNG TIN_SHOPEE HLV",
             "TOOL DEWALT",
-            "KHÁCH HÀNG KHÔNG CUNG CẤP THÔNG TIN_SHOPEE STANLEY"
+            "KHÁCH HÀNG KHÔNG CUNG CẤP THÔNG TIN_SHOPEE STANLEY",
+            "KHÁCH HÀNG KHÔNG CUNG CẤP THÔNG TIN_SHOPEE MILWAUKEE",
+            "KHÁCH HÀNG KHÔNG CUNG CẤP THÔNG TIN_SHOPEE DEWALT",
         }
 
     
