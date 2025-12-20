779a5c92c9bb97967795ef4e3d89c4a7a48ee235
TuPham <atu3012@gmail.com>
2025-12-20 09:11:42 +0700

    test

--- custom_addons/misa_fetch_po_button/models/sale_api_import_wizard.py
+++ custom_addons/misa_fetch_po_button/models/sale_api_import_wizard.py
@@ -786,9 +786,11 @@ class SaleApiImportWizard(models.TransientModel):
             "KHÁCH HÀNG KHÔNG CUNG CẤP THÔNG TIN_SHOPEE TBCN",
             "KHÁCH HÀNG KHÔNG CUNG CẤP THÔNG TIN_SHOPEE STANLEY",
             "KHÁCH HÀNG KHÔNG CUNG CẤP THÔNG TIN_TIKTOK",
-            "KHÁCH HÀNG KHÔNG CUNG CẤP THÔNG TIN_TIKTOK",
+            "KHÁCH HÀNG KHÔNG CUNG CẤP THÔNG TIN_SHOPEE HLV",
             "TOOL DEWALT",
-            "KHÁCH HÀNG KHÔNG CUNG CẤP THÔNG TIN_SHOPEE STANLEY"
+            "KHÁCH HÀNG KHÔNG CUNG CẤP THÔNG TIN_SHOPEE STANLEY",
+            "KHÁCH HÀNG KHÔNG CUNG CẤP THÔNG TIN_SHOPEE MILWAUKEE",
+            "KHÁCH HÀNG KHÔNG CUNG CẤP THÔNG TIN_SHOPEE DEWALT",
         }
 
     
