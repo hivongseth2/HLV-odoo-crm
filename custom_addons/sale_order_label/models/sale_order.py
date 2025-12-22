fa450017925541d04c02ac1a21222867cc697d9d
luannt@arthomevn.com <luannt@arthomevn.com>
2025-12-22 22:18:34 +0700

    sale order unique constrain by name

--- custom_addons/sale_order_label/models/sale_order.py
+++ custom_addons/sale_order_label/models/sale_order.py
@@ -2,6 +2,9 @@ from odoo import models
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
+    _sql_constraints = [
+        ('name_unique', 'unique(name)', 'Mã đơn hàng (Reference) đã tồn tại trong hệ thống!'),
+    ]
 
     def action_print_label(self):
         return self.env.ref('sale_order_label.action_report_sale_order_label').report_action(self)
