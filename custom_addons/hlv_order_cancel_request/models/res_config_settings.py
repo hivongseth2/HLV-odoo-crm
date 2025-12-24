dbeddec2797e899b968c0e3b6a768918b7b53d9f
TuPham <atu3012@gmail.com>
2025-12-24 15:41:50 +0700

    change label

--- custom_addons/hlv_order_cancel_request/models/res_config_settings.py
+++ custom_addons/hlv_order_cancel_request/models/res_config_settings.py
@@ -4,5 +4,5 @@ class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
     cancel_request_password = fields.Char(string='Mật khẩu trang web', config_parameter='hlv_order_cancel_request.website_password', help="Mật khẩu truy cập cho trang yêu cầu hủy đơn.")
-    cancel_request_accountant_uid = fields.Char(string='Zalo ID Kế Toán', config_parameter='hlv_order_cancel_request.accountant_zalo_uid', help="Zalo User ID của Kế toán để nhận thông báo.")
-    cancel_request_warehouse_uid = fields.Char(string='Zalo ID Thủ Kho', config_parameter='hlv_order_cancel_request.warehouse_zalo_uid', help="Zalo User ID của Thủ kho để nhận thông báo.")
+    cancel_request_accountant_uid = fields.Char(string='Zalo ID Thủ Kho', config_parameter='hlv_order_cancel_request.accountant_zalo_uid', help="Zalo User ID của Thủ Kho để nhận thông báo.")
+    cancel_request_warehouse_uid = fields.Char(string='Zalo ID Kế Toán', config_parameter='hlv_order_cancel_request.warehouse_zalo_uid', help="Zalo User ID của Kế toán để nhận thông báo.")
