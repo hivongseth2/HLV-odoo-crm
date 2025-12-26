59659c44e1bd9199ddc50ded4b7cee6c071aaadd
TuPham <atu3012@gmail.com>
2025-12-26 09:59:16 +0700

    test mở đơn bán hàng bằng mã đơn

--- custom_addons/sale_order_bulk_editor/models/sale_order.py
+++ custom_addons/sale_order_bulk_editor/models/sale_order.py
@@ -1,14 +1,23 @@
 # -*- coding: utf-8 -*-
-from odoo import models
+from odoo import models, fields, api
 
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
+    name_link = fields.Html(string='Số báo giá', compute='_compute_name_link')
+
+    @api.depends('name')
+    def _compute_name_link(self):
+        for record in self:
+            # Create a link to the form view of the order
+            url = f'/web#id={record.id}&model=sale.order&view_type=form'
+            # Use font-weight-bold to make it look like a primary field
+            record.name_link = f'<a href="{url}" target="_blank" class="font-weight-bold" style="color: #017e84;">{record.name}</a>'
+
     def action_view_order_detail(self):
         """
-        Mở form view chi tiết của đơn hàng.
-        Dùng khi list view đang ở chế độ editable và muốn xem chi tiết đơn.
+        Legacy method kept just in case, or can be removed if strictly cleaning up.
         """
         self.ensure_one()
         return {
