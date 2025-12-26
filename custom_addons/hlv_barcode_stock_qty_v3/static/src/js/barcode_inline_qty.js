9d87046dbcc4015a3e107f067c34ef3b3f684b38
luannt@arthomevn.com <luannt@arthomevn.com>
2025-12-26 13:45:09 +0700

    fix

--- custom_addons/hlv_barcode_stock_qty_v3/static/src/js/barcode_inline_qty.js
+++ custom_addons/hlv_barcode_stock_qty_v3/static/src/js/barcode_inline_qty.js
@@ -2,7 +2,7 @@
 
 // 1. SỬA IMPORT (Thêm ngoặc nhọn { } là quan trọng nhất)
 import  BarcodeModel  from "@stock_barcode/models/barcode_model";
-import { LineComponent } from "@stock_barcode/components/line"; // Component vẽ giao diện dòng
+import  LineComponent  from "@stock_barcode/components/line"; // Component vẽ giao diện dòng
 import { patch } from "@web/core/utils/patch";
 import { onMounted, onPatched } from "@odoo/owl"; // Hook của OWL
 
