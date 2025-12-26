3abf270c114dd75967608849ae3bf939e2ac3f22
TuPham <atu3012@gmail.com>
2025-12-26 13:22:11 +0700

    lại

--- custom_addons/hlv_pos_theme/__manifest__.py
+++ custom_addons/hlv_pos_theme/__manifest__.py
@@ -14,7 +14,7 @@
         # Using the standard asset bundle for Odoo 17/18 POS
         'point_of_sale._assets_pos': [
             'hlv_pos_theme/static/src/css/pos_theme.css',
-            'hlv_pos_theme/static/src/xml/Screens/ProductScreen/ProductsWidget.xml',
+            # 'hlv_pos_theme/static/src/xml/Screens/ProductScreen/ProductsWidget.xml', # Removed due to missing template error
             'hlv_pos_theme/static/src/js/category_button_patch.js',
         ],
     },
