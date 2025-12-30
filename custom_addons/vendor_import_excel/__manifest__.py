1ce73bb3061a3f8b2b11d3309c31dc4b489825e0
TuPham <atu3012@gmail.com>
2025-12-30 16:57:02 +0700

    feat: thêm module cập nhật thông tin nhà cung cấp

---
+++ custom_addons/vendor_import_excel/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Vendor Import Excel',
+    'version': '1.0',
+    'summary': 'Import/Update Vendors from Excel',
+    'description': """
+        Update vendor info from Excel file based on Name.
+        Updates: company_registry (ref), street, vat, phone, mobile.
+        Sets is_company=True if name contains "Công ty".
+    """,
+    'category': 'Tools',
+    'author': 'Antigravity',
+    'depends': ['base', 'contacts'],
+    'data': [
+        'security/ir.model.access.csv',
+        'wizard/vendor_import_wizard.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
