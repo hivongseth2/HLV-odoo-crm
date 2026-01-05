ecd7adfa44b08daac1bc6995d1196b0b19286b2b
TuPham <atu3012@gmail.com>
2026-01-05 14:42:06 +0700

    laij

--- custom_addons/hlv_pos_customer_type/static/src/js/payment_screen_patch.js
+++ custom_addons/hlv_pos_customer_type/static/src/js/payment_screen_patch.js
@@ -1,7 +1,6 @@
 /** @odoo-module */
 
 import { PaymentScreen } from "@point_of_sale/app/screens/payment_screen/payment_screen";
-import { ErrorPopup } from "@point_of_sale/app/errors/error_popup/error_popup";
 import { _t } from "@web/core/l10n/translation";
 import { patch } from "@web/core/utils/patch";
 
@@ -13,9 +12,11 @@ patch(PaymentScreen.prototype, {
 
         if (!partner) {
             console.log("DEBUG: No partner selected, blocking validation");
-            this.env.services.popup.add(ErrorPopup, {
-                title: _t('Customer Required'),
-                body: _t('Please select a customer to proceed with the order.'),
+            // Use notification instead of ErrorPopup to avoid import issues
+            this.env.services.notification.add(_t('Vui lòng chọn khách hàng để thanh toán.'), {
+                type: 'danger',
+                sticky: false,
+                title: _t('Yêu cầu khách hàng'),
             });
             return;
         }
