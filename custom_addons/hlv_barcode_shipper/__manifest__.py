16188e7638a1f9ddeb6cd63fa6a081c94d87d3f3
luannt@arthomevn.com <luannt@arthomevn.com>
2026-01-10 13:06:55 +0700

    add app to shipper

--- custom_addons/hlv_barcode_shipper/__manifest__.py
+++ custom_addons/hlv_barcode_shipper/__manifest__.py
@@ -18,6 +18,7 @@
         "views/stock_picking_views.xml",
         "views/barcode_shipper_views.xml",
         "views/res_config_settings_views.xml",
+        "views/menu_views.xml",
     ],
     "assets": {
         "web.assets_frontend": [
