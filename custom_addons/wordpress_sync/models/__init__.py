2a8535150a7e8995c5fdef6d00dd4c6b95005aee
luannt@arthomevn.com <luannt@arthomevn.com>
2026-01-26 11:00:27 +0700

    fix

--- custom_addons/wordpress_sync/models/__init__.py
+++ custom_addons/wordpress_sync/models/__init__.py
@@ -5,5 +5,6 @@ from . import product_template
 from . import wordpress_price_sync
 from . import wordpress_config
 from . import product_sync_log
+from . import res_config_settings
 from . import combo_stock_sync
 from . import wordpress_queue
