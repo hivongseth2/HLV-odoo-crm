685060211eb2cba8aebc3ee43621e7ba36d30c7b
luannt@arthomevn.com <luannt@arthomevn.com>
2026-02-06 14:27:48 +0700

    fix: remove broken partner_extension.py causing indentation error

--- custom_addons/zalo_chat_integration/models/__init__.py
+++ custom_addons/zalo_chat_integration/models/__init__.py
@@ -5,6 +5,5 @@ from . import zalo_chat_conversation
 from . import zalo_config
 from . import discuss_channel
 from . import res_partner
-from . import partner_extension
 from . import zalo_chat_evaluation
 from . import discuss_channel
