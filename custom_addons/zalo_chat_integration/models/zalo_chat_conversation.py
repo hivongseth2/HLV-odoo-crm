ffeadc1075d2d57cf6d3f5e41864452aee14e17d
luannt@arthomevn.com <luannt@arthomevn.com>
2026-02-06 08:55:03 +0700

    fix: handle UniqueViolation in _get_or_create_partner

--- custom_addons/zalo_chat_integration/models/zalo_chat_conversation.py
+++ custom_addons/zalo_chat_integration/models/zalo_chat_conversation.py
@@ -313,48 +313,62 @@ class ZaloChatConversation(models.Model):
         Uses full API response including shared_info
         Downloads and saves avatar image
         """
+        from psycopg2.errors import UniqueViolation
+        
         Partner = self.env['res.partner']
         
         # Search existing partner
         partner = Partner.search([('zalo_user_id', '=', zalo_user_id)], limit=1)
         
-        if not partner:
-            # Create new partner
-            name = user_info.get('display_name') or user_info.get('user_alias') or f'Zalo User {zalo_user_id[-4:]}'
-            partner_vals = {
-                'name': name,
-                'zalo_user_id': zalo_user_id,
-            }
-            
-            # Get shared_info if available
-            shared_info = user_info.get('shared_info', {})
-            
-            # Add phone from shared_info or user_id_by_app
-            phone = shared_info.get('phone') or user_info.get('user_id_by_app')
-            if phone and str(phone) != '0':
-                partner_vals['phone'] = str(phone)
-            
-            # Add address info if available
-            if shared_info.get('address'):
-                partner_vals['street'] = shared_info.get('address')
-            
-            if shared_info.get('city'):
-                partner_vals['city'] = shared_info.get('city')
-            
-            # Download and save avatar
-            avatar_url = user_info.get('avatar')
-            if not avatar_url and user_info.get('avatars'):
-                # Try to get from avatars dict (prefer 240px)
-                avatars = user_info.get('avatars', {})
-                avatar_url = avatars.get('240') or avatars.get('120')
-            
-            if avatar_url:
-                avatar_data = self._download_avatar(avatar_url)
-                if avatar_data:
-                    partner_vals['image_1920'] = avatar_data
-            
+        if partner:
+            return partner
+        
+        # Prepare partner values
+        name = user_info.get('display_name') or user_info.get('user_alias') or f'Zalo User {zalo_user_id[-4:]}'
+        partner_vals = {
+            'name': name,
+            'zalo_user_id': zalo_user_id,
+        }
+        
+        # Get shared_info if available
+        shared_info = user_info.get('shared_info', {})
+        
+        # Add phone from shared_info or user_id_by_app
+        phone = shared_info.get('phone') or user_info.get('user_id_by_app')
+        if phone and str(phone) != '0':
+            partner_vals['phone'] = str(phone)
+        
+        # Add address info if available
+        if shared_info.get('address'):
+            partner_vals['street'] = shared_info.get('address')
+        
+        if shared_info.get('city'):
+            partner_vals['city'] = shared_info.get('city')
+        
+        # Download and save avatar
+        avatar_url = user_info.get('avatar')
+        if not avatar_url and user_info.get('avatars'):
+            # Try to get from avatars dict (prefer 240px)
+            avatars = user_info.get('avatars', {})
+            avatar_url = avatars.get('240') or avatars.get('120')
+        
+        if avatar_url:
+            avatar_data = self._download_avatar(avatar_url)
+            if avatar_data:
+                partner_vals['image_1920'] = avatar_data
+        
+        try:
             partner = Partner.create(partner_vals)
             _logger.info(f'Created partner for Zalo user: {name} (ID: {zalo_user_id})')
+        except Exception as e:
+            # Handle race condition - another process created the partner
+            if 'unique' in str(e).lower() or 'duplicate' in str(e).lower():
+                self.env.cr.rollback()
+                partner = Partner.search([('zalo_user_id', '=', zalo_user_id)], limit=1)
+                if partner:
+                    _logger.info(f'Found existing partner after UniqueViolation: {partner.name}')
+                    return partner
+            raise
         
         return partner
     
