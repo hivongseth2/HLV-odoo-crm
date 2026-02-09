e37ef5442c60867e6750a03a3a8b46699a1e25aa
luannt@arthomevn.com <luannt@arthomevn.com>
2026-02-09 10:38:31 +0700

    fix

--- custom_addons/zalo_chat_integration/models/__init__.py
+++ custom_addons/zalo_chat_integration/models/__init__.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from . import zalo_chat_message
-from . import zalo_chat_conversation
 from . import zalo_config
 from . import discuss_channel
+from . import mail_message
 from . import res_partner
+from . import zalo_customer_tag
+from . import zalo_customer_profile
+from . import zalo_chat_conversation
+from . import zalo_chat_message
 from . import zalo_chat_evaluation
