ad49101a53250aabe226598e54b84dc1e9002f38
luannt@arthomevn.com <luannt@arthomevn.com>
2026-02-12 09:33:43 +0700

    ok

--- custom_addons/shopee_webhook/models/sale_order.py
+++ custom_addons/shopee_webhook/models/sale_order.py
@@ -5,5 +5,5 @@ from odoo import models, fields, api
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    # shopee_delivery_status = fields.Char(string='Shopee Delivery Status', help="Status received from Shopee Webhook")
+    shopee_order_status = fields.Char(string='Shopee Order Status', help="Status received from Shopee Webhook (e.g. PROCESSED, COMPLETED)")
     
