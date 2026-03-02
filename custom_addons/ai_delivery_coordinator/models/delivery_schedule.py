89e5afc05433af076018868dc22b556583769d39
luannt@arthomevn.com <luannt@arthomevn.com>
2026-03-02 11:33:21 +0700

    fix(delivery): add missing gettext import for action_open_kanban_board

--- custom_addons/ai_delivery_coordinator/models/delivery_schedule.py
+++ custom_addons/ai_delivery_coordinator/models/delivery_schedule.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields
+from odoo import models, fields, _
+from odoo.exceptions import UserError
+import urllib.parse
 
 class DeliveryRoute(models.Model):
     _name = 'delivery.route'
