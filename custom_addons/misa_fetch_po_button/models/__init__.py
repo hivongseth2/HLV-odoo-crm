0dad322f6a649dffa3345ae9f372c0251344c933
luannt@arthomevn.com <luannt@arthomevn.com>
2026-03-05 13:51:30 +0700

    hot fix

--- custom_addons/misa_fetch_po_button/models/__init__.py
+++ custom_addons/misa_fetch_po_button/models/__init__.py
@@ -8,4 +8,4 @@ from . import sale_order_misa_sync
 from . import sale_order_misa_id
 from . import crm_tag_misa
 from . import stock_picking_crm_delivery
-from . import product_misa_sync
+# from . import product_misa_sync
