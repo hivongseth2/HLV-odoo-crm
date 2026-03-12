26fd695d4f130f5e6e4ab0b6e732adc793f9a8bb
TuPham <atu3012@gmail.com>
2026-03-12 10:50:32 +0700

    test

--- custom_addons/shopee_webhook/models/sale_order.py
+++ custom_addons/shopee_webhook/models/sale_order.py
@@ -6,6 +6,3 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     shopee_order_status = fields.Char(string='Shopee Order Status', help="Status received from Shopee Webhook (e.g. PROCESSED, COMPLETED)")
-    
-    # Dummy field to fix crash from old Studio views
-    shopee_order_status_display = fields.Char(string='Shopee Order Status (Old)', help="Dummy field to fix crash")
