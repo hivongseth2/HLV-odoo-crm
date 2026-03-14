2f8f172d12695d8d8a053417af2be431aca6452a
luannt@arthomevn.com <luannt@arthomevn.com>
2026-03-14 11:28:39 +0700

    fix authen

--- custom_addons/hlv_sale_delivery_planning/controllers/sale_plan_controller.py
+++ custom_addons/hlv_sale_delivery_planning/controllers/sale_plan_controller.py
@@ -144,7 +144,7 @@ class SalePlanPublicController(http.Controller):
             )
             return False, 'not_configured'
         try:
-            uid = request.session.authenticate(request.db, VIEWER_LOGIN, viewer_pw)
+            uid = request.session.authenticate(VIEWER_LOGIN, viewer_pw)
             if uid:
                 return True, ''
             return False, 'bad_credentials'
