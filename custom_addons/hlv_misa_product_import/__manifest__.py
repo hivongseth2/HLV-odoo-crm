ebcf69628c03727efcb3038a9f449ce8354acff4
luannt@arthomevn.com <luannt@arthomevn.com>
2026-03-17 16:12:58 +0700

    fix : feat sync product

--- custom_addons/hlv_misa_product_import/__manifest__.py
+++ custom_addons/hlv_misa_product_import/__manifest__.py
@@ -1,13 +1,13 @@
 {
     'name': 'HLV MISA Product Import',
-    'version': '1.0',
+    'version': '1.1',
     'depends': ['base', 'product', 'point_of_sale', 'misa_fetch_po_button'],
     'author': 'HLV',
     'category': 'Inventory',
     'description': 'Import sản phẩm từ MISA CRM vào Odoo theo mã sản phẩm',
     'data': [
         'security/ir.model.access.csv',
-        'wizard/misa_product_import_wizard_views.xml',
+        'views/misa_product_import_views.xml',
     ],
     'installable': True,
     'auto_install': False,
