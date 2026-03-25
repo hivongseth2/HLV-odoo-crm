b964a967aa5e9f7ca1ba312f913a6778f832769a
TuPham <atu3012@gmail.com>
2026-03-25 16:01:20 +0700

    fix(google_ads): remove unnecessary advertising_channel_sub_type for Shopping campaigns

--- custom_addons/google_ads_automation/services/google_ads_mutate.py
+++ custom_addons/google_ads_automation/services/google_ads_mutate.py
@@ -120,7 +120,6 @@ class GoogleAdsMutateService:
             
             # Shopping campaign requirements
             if vals.get('channel_type') == 'SHOPPING':
-                campaign.advertising_channel_sub_type = client.enums.AdvertisingChannelSubTypeEnum.SHOPPING_PRODUCT_ADS
                 campaign.shopping_setting.merchant_id = int(vals.get('merchant_center_id'))
                 campaign.shopping_setting.campaign_priority = 0 # Low priority default
                 campaign.shopping_setting.sales_country = "VN" # Mandatory for many regions
