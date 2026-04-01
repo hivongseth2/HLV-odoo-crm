4765e40327a627a26af9f3608fb1e014c3d0ec21
TuPham <atu3012@gmail.com>
2026-04-01 15:14:14 +0700

    feat(google_ads): handle SMART channel type for campaign automation
    Add support for SMART campaign type by setting the required sub_type to 'SMART_CAMPAIGN'. This ensures Smart campaigns are properly configured when the channel type is set to 'SMART'.

--- custom_addons/google_ads_automation/models/google_ads_campaign.py
+++ custom_addons/google_ads_automation/models/google_ads_campaign.py
@@ -332,6 +332,8 @@ class GoogleAdsCampaign(models.Model):
             sub_type = self.video_sub_type
         elif self.channel_type == 'MULTI_CHANNEL':
             sub_type = self.app_sub_type
+        elif self.channel_type == 'SMART':
+            sub_type = 'SMART_CAMPAIGN' # Smart campaigns require this specific sub-type
 
         vals = {
             'name': self.name,
