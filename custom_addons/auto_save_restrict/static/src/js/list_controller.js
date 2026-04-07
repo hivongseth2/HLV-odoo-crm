534d118cf40bb9b0542d648055fb2a2e82fc1a80
TuPham <atu3012@gmail.com>
2026-04-07 14:45:03 +0700

    fix(auto_save_restrict): implement beforeUnload handlers for unsaved changes protection

--- custom_addons/auto_save_restrict/static/src/js/list_controller.js
+++ custom_addons/auto_save_restrict/static/src/js/list_controller.js
@@ -6,15 +6,26 @@ import { _t } from "@web/core/l10n/translation";
 import { SettingsConfirmationDialog } from "@web/webclient/settings_form_view/settings_confirmation_dialog";
 import { ConfirmationDialog } from "@web/core/confirmation_dialog/confirmation_dialog";
 import { ListConfirmationDialog } from "@web/views/list/list_confirmation_dialog";
+import { useDOMWindowEvent } from "@web/core/utils/hooks";
 
 patch(ListController.prototype, {
 /* Patch ListController to restrict auto save in tree views */
    setup(){
       super.setup(...arguments);
+      useDOMWindowEvent("beforeunload", this.beforeUnload);
       useSetupAction({
           beforeLeave: () => this.beforeLeave(),
 //          beforeUnload: (ev) => this.beforeUnload(ev),
       });
+   },
+   beforeUnload(ev) {
+      if(this.model.root.editedRecord){
+          ev.preventDefault();
+          ev.returnValue = "Unsaved changes";
+      }
+      if (super.beforeUnload) {
+          return super.beforeUnload(ev);
+      }
    },
    async beforeLeave() {
    /* function will work before leave the list */
