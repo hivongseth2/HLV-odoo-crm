bcca740f70036cefcb1aa025aa746e19aaf8ea59
TuPham <atu3012@gmail.com>
2026-04-07 15:58:18 +0700

    fix

--- custom_addons/shopee_webhook/controllers/main.py
+++ custom_addons/shopee_webhook/controllers/main.py
@@ -5,7 +5,7 @@ import logging
 from datetime import datetime
 from odoo import http
 from odoo.http import request
-from ..shopee_order_fetch.services import shopee_api, shopee_order_builder, shopee_escrow
+from odoo.addons.shopee_order_fetch.services import shopee_api, shopee_order_builder, shopee_escrow
 
 _logger = logging.getLogger(__name__)
 
