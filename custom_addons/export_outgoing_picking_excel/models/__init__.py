096f0995301ad41f0b1f66982b470589bf04cc11
hivongseth2 <hivongseth2@gmail.com>
2026-04-13 13:12:42 +0700

    remove export

--- custom_addons/export_outgoing_picking_excel/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import export_outgoing_picking_wizard
-from . import export_purchase_order_wizard
-from . import inventory_report_wizard
-from . import picking_export_shopee_wizard
-from . import stock_export_wizard
-from . import out_return_report_wizard
-from . import export_sales_report_wizard
