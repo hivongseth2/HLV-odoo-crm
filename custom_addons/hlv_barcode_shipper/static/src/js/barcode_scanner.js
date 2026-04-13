5238c80b1de4a4e46f974ec8162fdba660cd73b5
hivongseth2 <hivongseth2@gmail.com>
2026-04-13 17:35:31 +0700

    fix

--- custom_addons/hlv_barcode_shipper/static/src/js/barcode_scanner.js
+++ custom_addons/hlv_barcode_shipper/static/src/js/barcode_scanner.js
@@ -411,8 +411,8 @@ class BarcodeShipper {
             const stream = await navigator.mediaDevices.getUserMedia({
                 video: {
                     facingMode: { ideal: 'environment' },
-                    width: { ideal: 1920, min: 1280 },
-                    height: { ideal: 1080, min: 720 },
+                    width: { ideal: 1280 },
+                    height: { ideal: 720 },
                     focusMode: { ideal: 'continuous' },
                     frameRate: { ideal: 30 },
                 },
