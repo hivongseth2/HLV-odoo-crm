f5d42a78880c242d1c226e2319d8d3ae3c99c73c
hivongseth2 <hivongseth2@gmail.com>
2026-04-25 08:29:28 +0700

    fix

--- custom_addons/axenor_print_action_hide/static/src/js/action_manager_patch.js
+++ custom_addons/axenor_print_action_hide/static/src/js/action_manager_patch.js
@@ -2,19 +2,15 @@
 
 import { patch } from "@web/core/utils/patch";
 import { ActionMenus } from "@web/search/action_menus/action_menus";
+import { useService } from "@web/core/utils/hooks";
+import { rpc } from "@web/core/network/rpc";
 
 patch(ActionMenus.prototype, {
     async loadAvailablePrintItems() {
         const activeIds = this.props.getActiveIds();
         const resModel = this.props.resModel;
-        
-        console.log("=== axenor patch loadAvailablePrintItems ===");
-        console.log("resModel:", resModel);
-        console.log("activeIds:", activeIds);
-        console.log("props.items.print:", this.props.items.print);
 
         const originalItems = await super.loadAvailablePrintItems();
-        console.log("originalItems:", originalItems);
 
         if (!activeIds.length) {
             return originalItems;
@@ -29,13 +25,12 @@ patch(ActionMenus.prototype, {
 
         let bindings;
         try {
-            bindings = await this.orm.call(
-                "ir.actions.actions",
-                "get_bindings",
-                [resModel],
-                { context }
-            );
-            console.log("bindings:", bindings);
+            bindings = await rpc("/web/dataset/call_kw", {
+                model: "ir.actions.actions",
+                method: "get_bindings",
+                args: [resModel],
+                kwargs: { context },
+            });
         } catch (e) {
             console.warn("axenor: get_bindings failed", e);
             return originalItems;
