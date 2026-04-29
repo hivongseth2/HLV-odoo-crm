c5fc19b5fcac322bef0d3c8a59ef3e3f3ea1aac2
hivongseth2 <hivongseth2@gmail.com>
2026-04-29 15:13:04 +0700

    amis_callback: bump version 1.2 - add misa mapping fields to res.partner/product/uom/stock.move

--- custom_addons/amis_callback/__manifest__.py
+++ custom_addons/amis_callback/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "AMIS Callback Verified",
-    "version": "1.1",
+    "version": "1.2",
     "category": "Custom",
     "summary": "Nhận callback từ MISA, xác thực chữ ký SHA256 HMAC và xem log trong Odoo",
     "author": "ChatGPT",
