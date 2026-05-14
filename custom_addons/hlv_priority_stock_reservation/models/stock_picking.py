1084b80120629639c9e933f8be8393a89e8aa826
hivongseth2 <hivongseth2@gmail.com>
2026-05-14 12:06:38 +0700

    fix

--- custom_addons/hlv_priority_stock_reservation/models/stock_picking.py
+++ custom_addons/hlv_priority_stock_reservation/models/stock_picking.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
+import logging
 
 from odoo import models, fields, api, _
+
+_logger = logging.getLogger(__name__)
 
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
