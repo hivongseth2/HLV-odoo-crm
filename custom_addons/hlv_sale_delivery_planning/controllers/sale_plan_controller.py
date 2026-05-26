3baef8d81b07f8680461d80ed354c9a1e14ed173
hivongseth2 <hivongseth2@gmail.com>
2026-05-26 11:21:09 +0700

    change time pollin

--- custom_addons/hlv_sale_delivery_planning/controllers/sale_plan_controller.py
+++ custom_addons/hlv_sale_delivery_planning/controllers/sale_plan_controller.py
@@ -1563,7 +1563,7 @@ document.addEventListener('keydown',function(e){if(e.key==='Escape'){closeDrawer
 
 // --- Auto-refresh: poll for changes every 10s ---
 var _lastFingerprint=null;
-var _pollInterval=60000; // 10 seconds
+var _pollInterval=300000; // 5 minutes
 var _pollTimer=null;
 var _pollPaused=false;
 
