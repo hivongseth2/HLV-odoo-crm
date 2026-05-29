deedcea91099af2291ee3c3067d58b05c5fcf0ec
TuPham <atu3012@gmail.com>
2026-05-29 09:51:32 +0700

    fix: manually handle config parameter for hlv_barcode_show_qty_buttons
    The field definition was changed from using config_parameter attribute to custom get_values/set_values methods to ensure proper storage and retrieval of the setting. This fixes an issue where the setting might not have been persisted correctly.

--- custom_addons/hlv_mobile_barcode/models/res_config_settings.py
+++ custom_addons/hlv_mobile_barcode/models/res_config_settings.py
@@ -38,10 +38,19 @@ class ResConfigSettings(models.TransientModel):
     )
     hlv_barcode_show_qty_buttons = fields.Boolean(
         string='Hiển thị nút điều chỉnh số lượng nhanh',
-        config_parameter='hlv_mobile_barcode.hlv_barcode_show_qty_buttons',
-        default=True,
         help='Nếu bật, sẽ hiển thị các nút -10, -1, +1, +10 để chỉnh số lượng nhanh trong ứng dụng quét mã vạch.'
     )
+
+    def get_values(self):
+        res = super(ResConfigSettings, self).get_values()
+        res.update(
+            hlv_barcode_show_qty_buttons=self.env['ir.config_parameter'].sudo().get_param('hlv_mobile_barcode.hlv_barcode_show_qty_buttons', 'True') == 'True',
+        )
+        return res
+
+    def set_values(self):
+        super(ResConfigSettings, self).set_values()
+        self.env['ir.config_parameter'].sudo().set_param('hlv_mobile_barcode.hlv_barcode_show_qty_buttons', str(self.hlv_barcode_show_qty_buttons))
 
     def action_open_warehouse_permissions(self):
         self.ensure_one()
