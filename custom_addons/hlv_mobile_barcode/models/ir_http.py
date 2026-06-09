606137bfef2d9ef2d6a620b227dd7a05817ad858
hivongseth2 <hivongseth2@gmail.com>
2026-06-09 13:57:38 +0700

    fix : v2 errror can't call has_group

--- custom_addons/hlv_mobile_barcode/models/ir_http.py
+++ custom_addons/hlv_mobile_barcode/models/ir_http.py
@@ -12,6 +12,18 @@ class IrHttp(models.AbstractModel):
     _inherit = 'ir.http'
 
     @classmethod
+    def _user_in_group(cls, user, group_xmlid):
+        group = request.env.ref(group_xmlid, raise_if_not_found=False)
+        if not group or not user:
+            return False
+
+        user_groups = user.sudo().groups_id
+        if 'trans_implied_ids' in user_groups._fields:
+            user_groups |= user_groups.trans_implied_ids
+
+        return group in user_groups
+
+    @classmethod
     def _dispatch(cls, endpoint):
         path = request.httprequest.path.rstrip('/')
         uid = request.session.uid
@@ -20,7 +32,7 @@ class IrHttp(models.AbstractModel):
             (path == '/odoo/barcode' or path.startswith('/odoo/barcode/'))
             and user
             and uid != SUPERUSER_ID
-            and not user.has_group('hlv_mobile_barcode.group_stock_barcode_default_user')
+            and not cls._user_in_group(user, 'hlv_mobile_barcode.group_stock_barcode_default_user')
         ):
             _logger.warning(
                 "Blocked default Odoo barcode access for user %s (%s)",
