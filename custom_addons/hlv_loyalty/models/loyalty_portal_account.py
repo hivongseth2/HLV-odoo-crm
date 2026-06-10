d446f5abd19735a5f8339ff822bbfc5a090181e8
TuPham <atu3012@gmail.com>
2026-06-10 13:36:39 +0700

    filter lại

--- custom_addons/hlv_loyalty/models/loyalty_portal_account.py
+++ custom_addons/hlv_loyalty/models/loyalty_portal_account.py
@@ -27,7 +27,7 @@ class HlvLoyaltyPortalAccount(models.Model):
     partner_id = fields.Many2one(
         'res.partner', string='Khách hàng', required=True,
         ondelete='cascade', index=True,
-        domain=[('is_company', '=', True), ('parent_id', '=', False)],
+        domain=[('hlv_partner_type', 'in', ['root_company', 'root_person'])],
     )
     username = fields.Char(
         string='Tên đăng nhập', required=True, copy=False, index=True,
