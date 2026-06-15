0ebf9eed83f284e9842d2a9650dc8c61042ecda2
hivongseth2 <hivongseth2@gmail.com>
2026-06-15 14:41:44 +0700

    feat : loyalty add snap shot calculator , add simple noti

--- custom_addons/hlv_loyalty/models/res_company.py
+++ custom_addons/hlv_loyalty/models/res_company.py
@@ -17,6 +17,14 @@ class ResCompany(models.Model):
         string='Gửi thông báo tích điểm', default=True,
         help='Gửi email/thông báo cho khách khi được tích điểm',
     )
+    loyalty_notification_user_ids = fields.Many2many(
+        'res.users',
+        'res_company_hlv_loyalty_notification_user_rel',
+        'company_id',
+        'user_id',
+        string='Người nhận thông báo Loyalty',
+        help='Các user nội bộ nhận bus notification khi có yêu cầu đổi thưởng hoặc khách đổi quà.',
+    )
     loyalty_portal_default_password = fields.Char(
         string='Mật khẩu mặc định Portal',
         default='hlv@2026',
