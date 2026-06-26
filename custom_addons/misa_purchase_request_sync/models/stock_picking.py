bc12fc39bed3bfe9a9a5864f398bd0778e2bbeec
TuPham <atu3012@gmail.com>
2026-06-26 09:35:54 +0700

    fix: hide 'done' button on purchase request for non-approved states
    - Add import for stock_picking model
    - Update view to make button_done invisible unless state is 'approved' or 'in_progress'

---
+++ custom_addons/misa_purchase_request_sync/models/stock_picking.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from odoo import models
+
+class StockPicking(models.Model):
+    _inherit = "stock.picking"
+
+    def _action_done(self):
+        # Lưu kết quả gốc
+        res = super(StockPicking, self)._action_done()
+        
+        # Sau khi hoàn tất (done), nếu picking thuộc về một Đơn mua hàng (PO)
+        for picking in self:
+            if picking.purchase_id:
+                # Lấy danh sách các Purchase Request liên quan
+                prs = picking.purchase_id.order_line.mapped('purchase_request_lines.request_id')
+                for pr in prs:
+                    if pr.state in ('approved', 'in_progress'):
+                        # Kiểm tra xem TẤT CẢ các dòng của PR đã nhận đủ số lượng chưa
+                        all_done = True
+                        for line in pr.line_ids:
+                            if line.product_id.type == 'service':
+                                continue # Bỏ qua hàng dịch vụ
+                            # Nếu số lượng nhận thực tế (sau khi move done) nhỏ hơn yêu cầu ban đầu -> Chưa hoàn thành
+                            if line.product_qty > 0 and line.qty_done < line.product_qty:
+                                all_done = False
+                                break
+                        
+                        # Nếu đã hoàn thành toàn bộ, tự động click nút Hoàn thành cho PR
+                        if all_done:
+                            pr.sudo().button_done()
+
+        return res
