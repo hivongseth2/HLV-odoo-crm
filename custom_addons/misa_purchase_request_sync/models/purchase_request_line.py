e2252aa03954d8cf05eadbe74609392a5850c255
TuPham <atu3012@gmail.com>
2026-07-13 14:05:14 +0700

    feat: add bidirectional onchange for MISA tax and discount rates/amounts
    Replaced computed `misa_discount_amount` field with onchange methods that auto-calculate tax and discount amounts/rates bidirectionally. Also fixed encoding comment and whitespace.

--- custom_addons/misa_purchase_request_sync/models/purchase_request_line.py
+++ custom_addons/misa_purchase_request_sync/models/purchase_request_line.py
@@ -1,5 +1,6 @@
-# -*- coding: utf-8 -*-
+đê# -*- coding: utf-8 -*-
 from odoo import api, fields, models
+
 
 class PurchaseRequestLine(models.Model):
     _inherit = "purchase.request.line"
@@ -65,30 +66,53 @@ class PurchaseRequestLine(models.Model):
         help="Tổng trước thuế - Tiền chiết khấu + Tổng thuế"
     )
 
-    misa_discount_amount = fields.Float(
-        string="Tiền chiết khấu (MISA)",
-        compute="_compute_misa_financial_totals",
-        store=True,
-        digits='Product Price',
-        help="Số lượng * (Đơn giá trước thuế * TL chiết khấu / 100)"
-    )
-
-    @api.depends('product_qty', 'misa_price_before_tax', 'misa_tax_rate',
-                 'misa_discount_rate', 'misa_price_after_tax')
+    @api.depends('product_qty', 'misa_price_before_tax', 'misa_tax_amount',
+                 'misa_discount_amount', 'misa_price_after_tax')
     def _compute_misa_financial_totals(self):
         for line in self:
             before_tax = line.misa_price_before_tax or 0.0
             qty = line.product_qty or 0.0
-            tax_rate = line.misa_tax_rate or 0.0
-            discount_rate = line.misa_discount_rate or 0.0
+            tax_amount = line.misa_tax_amount or 0.0
+            discount_amount = line.misa_discount_amount or 0.0
             after_tax = line.misa_price_after_tax or 0.0
 
             before_tax_total = qty * before_tax
             line.misa_price_before_tax_total = before_tax_total
-            line.misa_tax_amount_total = qty * (before_tax * tax_rate / 100.0)
-            line.misa_discount_amount = qty * (before_tax * discount_rate / 100.0)
+            line.misa_tax_amount_total = qty * (before_tax * (line.misa_tax_rate or 0.0) / 100.0)
             line.misa_price_after_tax_total = qty * after_tax
-            line.misa_amount = before_tax_total - line.misa_discount_amount + line.misa_tax_amount_total
+            line.misa_amount = before_tax_total - discount_amount + tax_amount
+
+    # --- Onchange: tự động tính thuế 2 chiều ---
+    @api.onchange('misa_tax_rate')
+    def _onchange_misa_tax_rate(self):
+        if self.misa_tax_rate and self.misa_price_before_tax and self.product_qty:
+            self.misa_tax_amount = self.product_qty * self.misa_price_before_tax * self.misa_tax_rate / 100.0
+        elif self.misa_tax_rate == 0 and not self.misa_tax_amount:
+            self.misa_tax_amount = 0.0
+
+    @api.onchange('misa_tax_amount')
+    def _onchange_misa_tax_amount(self):
+        base = (self.misa_price_before_tax or 0.0) * (self.product_qty or 0.0)
+        if self.misa_tax_amount and base:
+            self.misa_tax_rate = self.misa_tax_amount / base * 100.0
+        elif self.misa_tax_amount == 0 and not self.misa_tax_rate:
+            self.misa_tax_rate = 0.0
+
+    # --- Onchange: tự động tính chiết khấu 2 chiều ---
+    @api.onchange('misa_discount_rate')
+    def _onchange_misa_discount_rate(self):
+        if self.misa_discount_rate and self.misa_price_before_tax and self.product_qty:
+            self.misa_discount_amount = self.product_qty * self.misa_price_before_tax * self.misa_discount_rate / 100.0
+        elif self.misa_discount_rate == 0 and not self.misa_discount_amount:
+            self.misa_discount_amount = 0.0
+
+    @api.onchange('misa_discount_amount')
+    def _onchange_misa_discount_amount(self):
+        base = (self.misa_price_before_tax or 0.0) * (self.product_qty or 0.0)
+        if self.misa_discount_amount and base:
+            self.misa_discount_rate = self.misa_discount_amount / base * 100.0
+        elif self.misa_discount_amount == 0 and not self.misa_discount_rate:
+            self.misa_discount_rate = 0.0
 
     @api.onchange("product_id")
     def onchange_product_id(self):
@@ -98,7 +122,7 @@ class PurchaseRequestLine(models.Model):
             supplier_info = self.env['product.supplierinfo'].search([
                 ('product_tmpl_id', '=', self.product_id.product_tmpl_id.id)
             ], limit=1, order='sequence, min_qty desc, price')
-            
+
             if supplier_info:
                 self.estimated_cost = supplier_info.price
             else:
@@ -106,7 +130,7 @@ class PurchaseRequestLine(models.Model):
                     ('product_id', '=', self.product_id.id),
                     ('state', 'in', ['purchase', 'done'])
                 ], limit=1, order='create_date desc')
-                
+
                 if last_po_line:
                     self.estimated_cost = last_po_line.price_unit
                 else:
