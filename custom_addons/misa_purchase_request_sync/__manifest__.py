f3833c11340eceba9d08abb3b2a568a5a8407bd1
TuPham <atu3012@gmail.com>
2026-07-15 13:56:19 +0700

    fix: bump version to 18.0.1.2.0 to trigger Odoo.sh module upgrade

--- custom_addons/misa_purchase_request_sync/__manifest__.py
+++ custom_addons/misa_purchase_request_sync/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     "name": "MISA Purchase Request Sync",
-    "version": "18.0.1.1.0",
+    "version": "18.0.1.2.0",
     "summary": "API endpoints + button to sync Purchase Request from MISA CRM Browser Extension",
     "description": """
 Module cung cấp:
