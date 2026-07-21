c82e0ae849061d5bec67d4b144673f483adbabe3
TuPham <atu3012@gmail.com>
2026-07-21 16:21:02 +0700

    feat(hlv_purchase_line_custom): add STT, production year, origin to PO lines
    Add automatic sequence number (STT), production year, and country of origin fields to purchase.order.line. Propagate these fields to stock.move and stock.move.line to maintain tracking consistency from purchase orders to incoming stock pickings.

---
+++ custom_addons/hlv_purchase_line_custom/models/stock_move.py
@@ -0,0 +1,45 @@
+from odoo import api, fields, models
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    stt = fields.Char(
+        string="STT",
+        compute="_compute_stt",
+        store=False,
+        help="Số thứ tự của dòng trong phiếu nhập kho / đơn mua",
+    )
+    production_year = fields.Char(
+        string="Năm sản xuất",
+        related="purchase_line_id.production_year",
+        store=True,
+        readonly=False,
+        help="Năm sản xuất được liên kết từ dòng đơn mua hàng",
+    )
+    country_of_origin = fields.Char(
+        string="Xuất xứ",
+        related="purchase_line_id.country_of_origin",
+        store=True,
+        readonly=False,
+        help="Xuất xứ được liên kết từ dòng đơn mua hàng",
+    )
+
+    @api.depends("purchase_line_id.stt", "picking_id.move_ids")
+    def _compute_stt(self):
+        for move in self:
+            if move.purchase_line_id and move.purchase_line_id.stt:
+                move.stt = move.purchase_line_id.stt
+            elif move.picking_id:
+                idx = 0
+                for m in move.picking_id.move_ids:
+                    if m.display_type:
+                        continue
+                    idx += 1
+                    if m == move:
+                        move.stt = str(idx)
+                        break
+                else:
+                    move.stt = False
+            else:
+                move.stt = False
