74d8f27b7b1330cf58c8513b09c8ed0750ad49c1
hivongseth2 <hivongseth2@gmail.com>
2026-08-22 11:10:58 +0700

    UI loyalty

--- custom_addons/hlv_loyalty/__manifest__.py
+++ custom_addons/hlv_loyalty/__manifest__.py
@@ -8,7 +8,7 @@
         - Sử dụng Voucher trên toàn hệ thống (cross-company)
         - Xử lý hoàn hàng / hủy Voucher / hết hạn
     """,
-    "version": "18.0.1.9.0",
+    "version": "18.0.1.10.0",
     "category": "Sales",
     "author": "HLV",
     "depends": ["sale_management", "stock", "mail", "website", "mrp"],
